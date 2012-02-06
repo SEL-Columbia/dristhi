cfcb40fdb6f050d33e85cf6cc5719d30a2151685
Aravind SV <arvind.sv@gmail.com>
2012-02-06 15:44:22 +0530

    Duck, Arindam | Fixing the submit listener.

--- drishti-mother-child/src/main/java/org/ei/scheduler/CommCareFormSubmitListener.java
+++ drishti-mother-child/src/main/java/org/ei/scheduler/CommCareFormSubmitListener.java
@@ -1,12 +1,13 @@
 package org.ei.scheduler;
 
+import org.ei.commcare.listener.event.CommCareFormEvent;
 import org.motechproject.model.MotechEvent;
 import org.motechproject.server.event.annotations.MotechListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommCareFormSubmitListener {
-    @MotechListener(subjects = {"Registration"})
+    @MotechListener(subjects = {CommCareFormEvent.EVENT_SUBJECT})
     public void handleX(MotechEvent event) {
         System.out.println(event);
     }
