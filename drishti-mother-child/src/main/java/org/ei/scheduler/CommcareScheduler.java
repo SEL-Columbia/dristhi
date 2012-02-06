268e2b624d28485fec3519324a9b8772057e386f
Arindam Bhattacharjee <topol.daiict@gmail.com>
2012-02-06 05:09:17 -0500

    duck/topol end to end listener with all the files

--- drishti-mother-child/src/main/java/org/ei/scheduler/CommcareScheduler.java
+++ drishti-mother-child/src/main/java/org/ei/scheduler/CommcareScheduler.java
@@ -1,4 +1,17 @@
 package org.ei.scheduler;
 
+import org.ei.commcare.listener.event.CommCareListener;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class CommcareScheduler {
+    public static void main(String[] args) throws Throwable {
+        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(
+                new String[] {"applicationContext-drishti.xml", "integrationCommon.xml"});
+        CommCareListener careListener = appContext.getBean(CommCareListener.class);
+        while (true) {
+            Thread.sleep(3000);
+            System.out.println("Hello! Going for it!");
+            careListener.fetchFromServer();
+        }
+    }
 }
