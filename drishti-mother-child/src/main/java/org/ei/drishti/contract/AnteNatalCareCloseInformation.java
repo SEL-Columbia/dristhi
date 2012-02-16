5cf0ed2a724f2950044374a4a0da899be5077325
Aravind SV <arvind.sv@gmail.com>
2012-02-16 17:11:45 +0530

    Duck | Added a test to make it hard to get the JSON for CommCare form definitions wrong.
    It checks that every form definition has the mappings part right, by
    making sure that the mappings can be converted into an object.

--- drishti-mother-child/src/main/java/org/ei/drishti/contract/AnteNatalCareCloseInformation.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/contract/AnteNatalCareCloseInformation.java
@@ -1,7 +1,10 @@
 package org.ei.drishti.contract;
 
+import java.util.Date;
+
 public class AnteNatalCareCloseInformation {
     private String caseId;
+    private Date dateOfDelivery;
 
     public AnteNatalCareCloseInformation(String caseId) {
         this.caseId = caseId;
