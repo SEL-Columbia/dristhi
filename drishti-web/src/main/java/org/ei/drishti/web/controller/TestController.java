9f902bac2c923f7154ea722b85bfd5b8303af5dc
Aravind SV <arvind.sv@gmail.com>
2012-02-20 16:42:06 +0530

    Duck | We can now test schedules, by changing time.

--- drishti-web/src/main/java/org/ei/drishti/web/controller/TestController.java
+++ drishti-web/src/main/java/org/ei/drishti/web/controller/TestController.java
@@ -1,16 +1,19 @@
 package org.ei.drishti.web.controller;
 
+import org.motechproject.util.DateUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
 @Controller
 public class TestController {
-    @RequestMapping("/test")
+    @RequestMapping("/time/set")
     @ResponseBody
-    public String test() throws IOException {
-        return "Hello World!";
+    public String setTime(@RequestParam("offset") Integer offsetInSeconds) throws IOException {
+        System.setProperty("faketime.offset.seconds", offsetInSeconds.toString());
+        return String.valueOf(DateUtil.now().getMillis());
     }
 }
