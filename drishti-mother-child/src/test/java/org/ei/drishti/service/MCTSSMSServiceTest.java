a5d0af81763bbaf4f22e951b35fa967cdbe70ef8
Aravind SV <arvind.sv@gmail.com>
2012-03-09 10:15:27 +0530

    Duck | Renamed a variable in a test.

--- drishti-mother-child/src/test/java/org/ei/drishti/service/MCTSSMSServiceTest.java
+++ drishti-mother-child/src/test/java/org/ei/drishti/service/MCTSSMSServiceTest.java
@@ -12,24 +12,24 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class MCTSSMSServiceTest {
     @Mock
     SmsService smsService;
-    private MCTSSMSService MCTSSMSService;
+    private MCTSSMSService mctsSMSService;
 
     @Before
     public void setUp() throws Exception {
         initMocks(this);
-        MCTSSMSService = new MCTSSMSService(smsService);
+        mctsSMSService = new MCTSSMSService(smsService);
     }
 
     @Test
     public void shouldSendSMSBasedOnCodeForANC1() {
-        MCTSSMSService.send(MCTSServiceCode.ANC_1, "1234567", DateUtil.newDate(2012, 3, 21));
+        mctsSMSService.send(MCTSServiceCode.ANC_1, "1234567", DateUtil.newDate(2012, 3, 21));
 
         verify(smsService).sendSMS("9243355223", "ANMPW 1234567 ANC1 210312");
     }
 
     @Test
     public void shouldSendSMSWithZeroPaddedDateWhenDateIsBefore10th() {
-        MCTSSMSService.send(MCTSServiceCode.ANC_1, "1234567", DateUtil.newDate(2012, 3, 8));
+        mctsSMSService.send(MCTSServiceCode.ANC_1, "1234567", DateUtil.newDate(2012, 3, 8));
 
         verify(smsService).sendSMS("9243355223", "ANMPW 1234567 ANC1 080312");
     }
