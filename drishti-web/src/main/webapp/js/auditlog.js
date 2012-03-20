3d55368dd914effb9d69c32fe146fe6de84eed5d
Aravind SV <arvind.sv@gmail.com>
2012-03-20 12:27:53 +0530

    Duck | Audit log UI started. Looks fine for now.

---
+++ drishti-web/src/main/webapp/js/auditlog.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    var previousIndexOfMessages = 0;
+
+    var statusDisplay = function(status) {
+        $('#status').text(status);
+    };
+    var clearStatusDisplay = function() {
+        $('#status').text("");
+    };
+
+    var showNewMessages = function(newMessages) {
+        $(newMessages).each(function(index, elem) { $("#messages").prepend(
+        "<div class=\"audit_item\">" +
+            "<span class=\"time\">" + new Date(elem.time) + "</span>" +
+            "<span class=\"message\">" + elem.message.replace(/","/g, "\", \"").replace(/":"/g, "\": \"") + "</span>" +
+        "</div>"); });
+    };
+
+    var updateMessageIndex = function(newMessages) {
+        if (newMessages.length == 0) return;
+        previousIndexOfMessages = newMessages[newMessages.length - 1].index;
+    };
+
+    var displayNewMessages = function() {
+        $.getJSON('../audit/messages', {previousAuditMessageIndex: previousIndexOfMessages})
+            .success(function(responseFromServer) {
+                clearStatusDisplay();
+                updateMessageIndex(responseFromServer);
+                showNewMessages(responseFromServer);
+            })
+            .error(function(jqXHR, textStatus) {
+                statusDisplay("Failed!");
+            });
+
+    };
+
+    var init = function() {
+        setInterval(displayNewMessages, 2000);
+    };
+
+    init();
+});
