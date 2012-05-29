894726a384dade672da17fc4e471cd398da16c86
Harshit Agarwal <harshita@thoughtworks.com>
2012-05-29 17:00:51 +0530

    Harshit & Duck | Changed keys again. Have a new client keystore for Android now.

--- assets/security/generate-keys.sh
+++ assets/security/generate-keys.sh
@@ -7,8 +7,6 @@ fi
 
 prefix=$1
 
-echo -e "====== PLEASE make sure that you enter the same password every time it is asked here. ====="
-
 echo -e "Generating the keyfile. You will need to enter a password here."
 openssl genrsa -des3 -out ${prefix}.server.key 1024
 
@@ -24,14 +22,20 @@ openssl x509 -req -days 1095 -in ${prefix}.server.csr -signkey ${prefix}.server.
 
 echo -e "\nCombining the key and certificate and creating a keystore, so that Jetty can use it."
 openssl pkcs12 -inkey ${prefix}.server.key -in ${prefix}.server.crt -export -out ${prefix}.pkcs12
-keytool -importkeystore -srckeystore ${prefix}.pkcs12 -srcstoretype PKCS12 -destkeystore ${prefix}.keystore
+keytool -importkeystore -srckeystore ${prefix}.pkcs12 -srcstoretype PKCS12 -destkeystore ${prefix}_server.keystore
+
+echo -e "\n === === === Almost done. Please enter a DIFFERENT password from the one you entered earlier. This is for clients. === === ==="
+export CLASSPATH=bcprov-jdk15on-146.jar
+keytool -import -v -trustcacerts -alias ${prefix} -file <(openssl x509 -in ${prefix}.server.crt) -keystore ${prefix}_client.keystore -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath ./bcprov-jdk15on-146.jar
 
 cat <<EOF
 
 Take a look at: http://www.akadia.com/services/ssh_test_certificate.html if you need any more info.
 
 If you have come till here, these are the files you'll probably care about:
-Keystore for Jetty : ${prefix}.keystore
-SSL Private Key    : ${prefix}.server.key
-SSL Certificate    : ${prefix}.server.crt
+Keystore for Jetty/Tomcat : ${prefix}_server.keystore
+Keystore for clients      : ${prefix}_client.keystore
+SSL Private Key           : ${prefix}.server.key
+SSL Certificate           : ${prefix}.server.crt
 EOF
+
