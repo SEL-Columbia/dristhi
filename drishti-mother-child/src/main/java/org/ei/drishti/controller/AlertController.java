8991d282ac26a9dd632a8a3f3a67235661c5845c
Harshit Agarwal <harshita@thoughtworks.com>
2012-08-24 14:04:01 +0530

    Harshit & Duck | Ignoring alerts for child now.

--- drishti-mother-child/src/main/java/org/ei/drishti/controller/AlertController.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/controller/AlertController.java
@@ -3,9 +3,12 @@ package org.ei.drishti.controller;
 import org.ei.drishti.scheduler.router.Action;
 import org.ei.drishti.scheduler.router.AlertRouter;
 import org.ei.drishti.scheduler.router.Matcher;
+import org.ei.drishti.scheduler.router.MilestoneEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 import static org.ei.drishti.scheduler.DrishtiSchedules.*;
 import static org.ei.drishti.scheduler.router.Matcher.any;
@@ -23,7 +26,7 @@ public class AlertController {
         router.addRoute(eq(SCHEDULE_ANC), any(), eq(max.toString()), forceFulfill);
         router.addRoute(eq(SCHEDULE_LAB), any(), eq(max.toString()), forceFulfill);
         router.addRoute(motherSchedules(), any(), anyOf(due.toString(), late.toString()), alertCreation).addExtraData("beneficiaryType", "mother");
-        router.addRoute(childSchedules(), any(), anyOf(due.toString(), late.toString()), alertCreation).addExtraData("beneficiaryType", "child");
+        router.addRoute(childSchedules(), any(), anyOf(due.toString(), late.toString()), doNothingAction()).addExtraData("beneficiaryType", "child");
         router.addRoute(any(), any(), any(), anmGroupSMS);
     }
 
@@ -33,5 +36,13 @@ public class AlertController {
 
     private Matcher motherSchedules() {
         return anyOf(SCHEDULE_ANC, SCHEDULE_TT, SCHEDULE_IFA, SCHEDULE_LAB, SCHEDULE_EDD);
+    }
+
+    private Action doNothingAction() {
+        return new Action() {
+            @Override
+            public void invoke(MilestoneEvent event, Map<String, String> extraData) {
+            }
+        };
     }
 }
--- drishti-mother-child/src/test/java/org/ei/drishti/controller/AlertControllerRoutesTest.java
+++ drishti-mother-child/src/test/java/org/ei/drishti/controller/AlertControllerRoutesTest.java
@@ -43,17 +43,17 @@ public class AlertControllerRoutesTest {
     }
 
     @Test
-    public void shouldSendAllRemindersOfAllChildSchedulesToCaptureRemindersAction() throws Exception {
-        Event.of(CHILD_SCHEDULE_BCG, "REMINDER", WindowName.due).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_BCG, "REMINDER", WindowName.late).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_DPT, "DPT 1", WindowName.due).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_DPT, "DPT 1", WindowName.late).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_HEPATITIS, "Hepatitis B3", WindowName.due).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_HEPATITIS, "Hepatitis B3", WindowName.late).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_MEASLES, "REMINDER", WindowName.due).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_MEASLES, "REMINDER", WindowName.late).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_OPV, "OPV 1", WindowName.due).shouldRouteToAlertCreationActionForChild();
-        Event.of(CHILD_SCHEDULE_OPV, "OPV 1", WindowName.late).shouldRouteToAlertCreationActionForChild();
+    public void shouldDoNothingForAllChildSchedules() throws Exception {
+        Event.of(CHILD_SCHEDULE_BCG, "REMINDER", WindowName.due).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_BCG, "REMINDER", WindowName.late).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_DPT, "DPT 1", WindowName.due).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_DPT, "DPT 1", WindowName.late).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_HEPATITIS, "Hepatitis B3", WindowName.due).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_HEPATITIS, "Hepatitis B3", WindowName.late).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_MEASLES, "REMINDER", WindowName.due).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_MEASLES, "REMINDER", WindowName.late).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_OPV, "OPV 1", WindowName.due).shouldDoNothing();
+        Event.of(CHILD_SCHEDULE_OPV, "OPV 1", WindowName.late).shouldDoNothing();
     }
 
     private static class Event {
@@ -83,6 +83,10 @@ public class AlertControllerRoutesTest {
             Map<String, String> extraData = new HashMap<>();
             extraData.put("beneficiaryType", "mother");
             expectCalls(Expectation.of(0), Expectation.of(0), Expectation.of(1, extraData));
+        }
+
+        public void shouldDoNothing() {
+            expectCalls(Expectation.of(0), Expectation.of(0), Expectation.of(0));
         }
 
         public void shouldRouteToAlertCreationActionForChild() {
