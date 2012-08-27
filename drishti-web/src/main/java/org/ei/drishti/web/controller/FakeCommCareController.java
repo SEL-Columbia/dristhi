b1ac5bf36417ae867fb097949cb5a8db797d34a4
Aravind SV <arvind.sv@gmail.com>
2012-08-27 09:26:08 +0530

    Harshit & Duck | Putting in a way of forcing the CommCare scheduler to run.

---
+++ drishti-web/src/main/java/org/ei/drishti/web/controller/FakeCommCareController.java
@@ -0,0 +1,28 @@
+package org.ei.drishti.web.controller;
+
+import org.ei.drishti.scheduler.CommCareScheduler;
+import org.motechproject.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@Controller
+public class FakeCommCareController {
+    private CommCareScheduler commCareScheduler;
+
+    @Autowired
+    public FakeCommCareController(CommCareScheduler commCareScheduler) {
+        this.commCareScheduler = commCareScheduler;
+    }
+
+    @RequestMapping("/scheduler/run")
+    @ResponseBody
+    public String runScheduler() throws Exception {
+        commCareScheduler.fetchFromCommCareHQ(null);
+        return "Done";
+    }
+}
