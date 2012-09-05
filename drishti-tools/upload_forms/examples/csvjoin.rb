e1202196b68dee807642e8c8ae44fe88c8d2583f
Aravind SV <arvind.sv@gmail.com>
2012-09-05 16:30:48 +0530

    Duck | Adding prefix support for CSV join utility.

--- drishti-tools/upload_forms/examples/csvjoin.rb
+++ drishti-tools/upload_forms/examples/csvjoin.rb
@@ -2,12 +2,30 @@
 
 require 'csv'
 
-(STDERR.puts "Usage: csvjoin.rb CSV-FILE-1.csv CSV-FILE-2.csv column-in-file-1=column-in-file2 [column-in-file-1=column-in-file2]"; exit 1) if $ARGV.length < 3
-(STDERR.puts "File: #{$ARGV[0]} does not exist."; exit 1) if not File.exists? $ARGV[0]
-(STDERR.puts "File: #{$ARGV[1]} does not exist."; exit 1) if not File.exists? $ARGV[1]
+error_message = %q(Usage: csvjoin.rb [PREFIX-FOR-CSV1=]CSV-FILE-1.csv [PREFIX-FOR-CSV2=]CSV-FILE-2.csv column-in-file-1=column-in-file2 [column-in-file-1=column-in-file2]
 
-first_csv = CSV.read(ARGV[0], {headers: true})
-second_csv = CSV.read(ARGV[1], {headers: true})
+For example: csvjoin.rb EC_=ec_details.csv anc_details.csv number=ec_number village=village
+
+This will give back a CSV with rows where the "number" column in ec_details.csv is the same as "ec_number" column in
+anc_details.csv AND the "village" column in both CSV files is the same.
+
+Also, in this case, the headers of the ec_details.csv will be prefixed with "EC_" (optional prefix has been provided).
+Something like this:
+
+EC_number,EC_village,EC_othercolumn,ec_number,village,othercolumn
+1,"Village 1","Some other value",1,"Village 1","Some value in ANC"
+2,"Village 2","Some value",2,"Village 2","Some value")
+
+(STDERR.puts error_message; exit 1) if $ARGV.length < 3
+
+(prefix_for_first_csv, filename_for_first_csv) = ($ARGV[0] =~ /=/ ? $ARGV[0] : "=" + $ARGV[0]).split(/=/)
+(prefix_for_second_csv, filename_for_second_csv) = ($ARGV[1] =~ /=/ ? $ARGV[1] : "=" + $ARGV[1]).split(/=/)
+
+(STDERR.puts "File: #{filename_for_first_csv} does not exist."; exit 1) if not File.exists? filename_for_first_csv
+(STDERR.puts "File: #{filename_for_second_csv} does not exist."; exit 1) if not File.exists? filename_for_second_csv
+
+first_csv = CSV.read(filename_for_first_csv, {headers: true})
+second_csv = CSV.read(filename_for_second_csv, {headers: true})
 
 all_columns_to_match = []
 $ARGV[2..-1].each do |column_arg|
@@ -19,7 +37,7 @@ $ARGV[2..-1].each do |column_arg|
 end
 
 output_csv = CSV.generate do |output|
-  output << (first_csv.headers + second_csv.headers)
+  output << (first_csv.headers.collect {|header| prefix_for_first_csv + header} + second_csv.headers.collect {|header| prefix_for_second_csv + header})
 
   first_csv.each do |row_in_first_csv|
     second_csv.each do |row_in_second_csv|
