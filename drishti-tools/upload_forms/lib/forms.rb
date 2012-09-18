a10eaef5c6db8567a598445e5291c7603d5cd5ca
Aravind SV <arvind.sv@gmail.com>
2012-09-18 13:49:07 +0530

    Duck | Printing out the linked EC, ANCs and ANC services.

---
+++ drishti-tools/upload_forms/lib/forms.rb
@@ -0,0 +1,49 @@
+class Forms
+  def initialize ec_data, anc_data, anc_services_data
+    @ec = ec_data
+    @ancs = anc_data
+    @anc_services = anc_services_data
+  end
+
+  def fill
+    fill_ec_form
+    fill_anc_registration_forms if has_anc?
+    fill_anc_services_forms if has_anc? and has_services?
+    fill_anc_outcome_forms if has_anc? and has_services? and has_outcome?
+  end
+
+  private
+  def fill_ec_form
+    puts "EC: #{@ec['Wife Name']} - #{@ec['Husband Name']} - #{@ec['Case ID']}"
+  end
+
+  def fill_anc_registration_forms
+    @ancs.each do |anc|
+      puts "    ANC registration: #{anc['LMP']} - #{anc['Case ID']}"
+    end
+  end
+
+  def fill_anc_services_forms
+    @anc_services.each do |anc_service|
+      puts "        ANC service: Visit number: #{anc_service['Visit Number']}"
+    end
+  end
+
+  def fill_anc_outcome_forms
+    anc_service = @anc_services.first
+    puts "    Have ANC outcome: On #{anc_service['Date of Delivery']}. Result: #{anc_service['Outcomes']}"
+  end
+
+  def has_anc?
+    not @ancs.nil?
+  end
+
+  def has_services?
+    not @anc_services.nil?
+  end
+
+  def has_outcome?
+    @anc_services.any? {|service| not service['Date of Delivery'].nil? and not service['Date of Delivery'].empty? }
+  end
+end
+
--- drishti-tools/upload_forms/upload_forms.rb
+++ drishti-tools/upload_forms/upload_forms.rb
@@ -1,9 +1,16 @@
 require_relative 'read_anc_services_from_xlsx.rb'
 require_relative 'read_ancs_from_xlsx.rb'
 require_relative 'read_ecs_from_xlsx.rb'
+require_relative 'lib/forms.rb'
 
 ecs = ECs.new("examples/Munjanhalli.xlsx").ecs
 ancs_per_ec = ANCs.new("examples/Munjanhalli.xlsx").ancs_grouped_per_couple
 anc_services_per_ec = ANCServices.new("examples/Munjanhalli.xlsx").anc_services_per_ec
 
 puts "Got: ECs: #{ecs.size}. ANCS grouped by EC: #{ancs_per_ec.size}. Services grouped by EC: #{anc_services_per_ec.size}"
+
+ecs.each do |ec|
+  key = [ec['Village Code'].village, ec['Wife Name'], ec['Husband Name']]
+
+  Forms.new(ec, ancs_per_ec[key], anc_services_per_ec[key]).fill
+end
