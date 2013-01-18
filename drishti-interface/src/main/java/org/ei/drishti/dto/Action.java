8ec0cbd6eba731dc9601cadae36e1cf8b14ca1cd
Harshit Agarwal <harshita@thoughtworks.com>
2013-01-18 17:04:37 +0530

    Harshit | Upgraded apache-commons on interface.

--- drishti-interface/src/main/java/org/ei/drishti/dto/Action.java
+++ drishti-interface/src/main/java/org/ei/drishti/dto/Action.java
@@ -1,8 +1,8 @@
 package org.ei.drishti.dto;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import java.util.Map;
--- drishti-interface/src/main/java/org/ei/drishti/dto/ActionData.java
+++ drishti-interface/src/main/java/org/ei/drishti/dto/ActionData.java
@@ -1,8 +1,8 @@
 package org.ei.drishti.dto;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 
