6028fe107606104e641b4cce60d46eb70566e755
Harshit Agarwal <harshita@thoughtworks.com>
2013-04-09 18:24:18 +0530

    Harshit | Refactored DFL service.

--- drishti-mother-child/src/main/java/org/ei/drishti/service/DFLService.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/DFLService.java
@@ -1,7 +1,10 @@
 package org.ei.drishti.service;
 
 import org.ei.drishti.repository.FormDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.script.Bindings;
@@ -16,6 +19,7 @@ import static javax.script.ScriptContext.ENGINE_SCOPE;
 
 @Service
 public class DFLService {
+    private static Logger logger = LoggerFactory.getLogger(DFLService.class.toString());
     private static final String SAVE_METHOD_NAME = "save";
     private static final String JS_INIT_SCRIPT = "var formDataRepository = new enketo.FormDataRepository();\n" +
             "    var controller = new enketo.FormDataController(\n" +
@@ -23,40 +27,58 @@ public class DFLService {
             "        new enketo.FormDefinitionLoader(),\n" +
             "        new enketo.FormModelMapper(formDataRepository, new enketo.SQLQueryBuilder(formDataRepository), new enketo.IdFactory(new enketo.IdFactoryBridge())),\n" +
             "        formDataRepository);";
+    private static final String JSON_FILE_LOADER = "jsonFileLoader";
+    private static final String REPOSITORY = "repository";
+
     private JSONFileLoader jsonFileLoader;
     private FormDataRepository dataRepository;
+    private Object dflFormController;
+    private Invocable invocable;
 
     @Autowired
-    public DFLService(JSONFileLoader jsonFileLoader, FormDataRepository dataRepository) {
+    public DFLService(JSONFileLoader jsonFileLoader, FormDataRepository dataRepository, @Value("#{drishti['js.files.path']}") String jsFilesPath) {
         this.jsonFileLoader = jsonFileLoader;
         this.dataRepository = dataRepository;
+        initRhino(jsFilesPath);
     }
 
     public void saveForm(String params, String formInstance) {
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("JavaScript");
-
         try {
-            Bindings bindings = engine.createBindings();
-            bindings.put("jsonFileLoader", jsonFileLoader);
-            bindings.put("repository", dataRepository);
-            engine.setBindings(bindings, ENGINE_SCOPE);
-
-            String jsFiles = getJSFiles();
-
-            engine.eval(jsFiles);
-            engine.eval(JS_INIT_SCRIPT);
-
-            Object formDataRepository = engine.get("controller");
-            Invocable invocable = (Invocable) engine;
-            invocable.invokeMethod(formDataRepository, SAVE_METHOD_NAME, params, formInstance);
+            invocable.invokeMethod(dflFormController, SAVE_METHOD_NAME, params, formInstance);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private String getJSFiles() throws IOException {
-        File jsFolder = new File("drishti-mother-child/js");
+    private void initRhino(String jsFilesPath) {
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+            Bindings bindings = getBindings(engine);
+            engine.setBindings(bindings, ENGINE_SCOPE);
+
+            String jsFiles = getJSFiles(jsFilesPath);
+
+            engine.eval(jsFiles);
+            engine.eval(JS_INIT_SCRIPT);
+
+            dflFormController = engine.get("controller");
+            invocable = (Invocable) engine;
+        } catch (Exception e) {
+            logger.error("Rhino initialization failed: " + e);
+        }
+    }
+
+    private Bindings getBindings(ScriptEngine engine) {
+        Bindings bindings = engine.createBindings();
+        bindings.put(JSON_FILE_LOADER, jsonFileLoader);
+        bindings.put(REPOSITORY, dataRepository);
+        return bindings;
+    }
+
+    private String getJSFiles(String jsFilesPath) throws IOException {
+        File jsFolder = new File(jsFilesPath);
         File[] files = jsFolder.listFiles(new FileFilter() {
             @Override
             public boolean accept(File file) {
--- drishti-mother-child/src/main/java/org/ei/drishti/service/JSONFileLoader.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/JSONFileLoader.java
@@ -1,14 +1,25 @@
 package org.ei.drishti.service;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Component
 public class JSONFileLoader {
+    private String formFilePath;
+
+    @Autowired
+    public JSONFileLoader(@Value("#{drishti['form.files.path']}") String formFilePath) {
+        this.formFilePath = formFilePath;
+    }
+
     public String loadFormData(String fileName) throws IOException {
-        File file = new File("drishti-mother-child/form/" + fileName);
+        File file = new File(formFilePath + fileName);
         return load(file);
     }
 
