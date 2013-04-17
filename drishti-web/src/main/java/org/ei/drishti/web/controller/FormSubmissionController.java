3888bd6e4f17e8f4d62ed4342796bc208d113ecc
Harshit Agarwal <harshita@thoughtworks.com>
2013-04-17 15:14:52 +0530

    Harshit | Fixed the FormSubmissionController to return proper status code.

--- drishti-web/src/main/java/org/ei/drishti/web/controller/FormSubmissionController.java
+++ drishti-web/src/main/java/org/ei/drishti/web/controller/FormSubmissionController.java
@@ -6,7 +6,9 @@ import org.motechproject.scheduler.gateway.OutboundEventGateway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,15 +31,18 @@ public class FormSubmissionController {
     }
 
     @RequestMapping(headers = {"Accept=application/json"}, method = POST)
-    @ResponseBody
-    public HttpStatus submitForms(@RequestBody List<FormSubmission> formSubmissions) {
+    public ResponseEntity<HttpStatus> submitForms(@RequestBody List<FormSubmission> formSubmissions) {
         try {
+            if (formSubmissions.isEmpty()) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+
             gateway.sendEventMessage(new FormSubmissionEvent(formSubmissions).toEvent());
             logger.info(format("Added Form submissions to queue.\nSubmissions: {0}", formSubmissions));
         } catch (Exception e) {
             logger.error(format("Form submissions processing failed with exception {0}.\nSubmissions: {1}", e, formSubmissions));
-            return HttpStatus.INTERNAL_SERVER_ERROR;
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return HttpStatus.CREATED;
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
