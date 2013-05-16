eabac80b8a2e6e5ec31131ee467b0ce20298ec9c
Kiran <kirang20@gmail.com>
2013-05-16 11:36:35 +0530

    Kiran | Added InProcess as another alertPriority.

--- drishti-interface/src/main/java/org/ei/drishti/dto/AlertPriority.java
+++ drishti-interface/src/main/java/org/ei/drishti/dto/AlertPriority.java
@@ -1,7 +1,7 @@
 package org.ei.drishti.dto;
 
 public enum AlertPriority {
-    upcoming("upcoming"), normal("normal"), urgent("urgent");
+    upcoming("upcoming"), normal("normal"), urgent("urgent"), inProcess("inProcess");
 
     private String value;
 
