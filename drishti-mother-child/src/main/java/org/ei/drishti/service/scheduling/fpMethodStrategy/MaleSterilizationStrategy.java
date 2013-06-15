01b70dd38b3cc343eda49dc92829963c65c244c7
Kiran <kirang20@gmail.com>
2013-06-15 11:28:01 +0530

    Kiran | Moved usages of MaleSterilizationStrategy enrollment to new ScheduleService.

--- drishti-mother-child/src/main/java/org/ei/drishti/service/scheduling/fpMethodStrategy/MaleSterilizationStrategy.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/scheduling/fpMethodStrategy/MaleSterilizationStrategy.java
@@ -3,8 +3,7 @@ package org.ei.drishti.service.scheduling.fpMethodStrategy;
 import org.ei.drishti.contract.Schedule;
 import org.ei.drishti.domain.FPProductInformation;
 import org.ei.drishti.service.ActionService;
-import org.motechproject.model.Time;
-import org.motechproject.scheduletracking.api.service.EnrollmentRequest;
+import org.ei.drishti.service.scheduling.ScheduleService;
 import org.motechproject.scheduletracking.api.service.ScheduleTrackingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Component;
 import static java.text.MessageFormat.format;
 import static java.util.Arrays.asList;
 import static org.ei.drishti.scheduler.DrishtiScheduleConstants.ECSchedulesConstants.*;
-import static org.ei.drishti.scheduler.DrishtiScheduleConstants.PREFERED_TIME_FOR_SCHEDULES;
 import static org.joda.time.LocalDate.parse;
 
 @Component
@@ -22,14 +20,17 @@ public class MaleSterilizationStrategy implements FPMethodStrategy {
     private static Logger logger = LoggerFactory.getLogger(MaleSterilizationStrategy.class.toString());
     private final ScheduleTrackingService scheduleTrackingService;
     private final ActionService actionService;
+    private final ScheduleService scheduleService;
     private final Schedule maleSterilizationFollowupSchedule = new Schedule(EC_SCHEDULE_MALE_STERILIZATION_FOLLOWUP,
             asList(EC_SCHEDULE_MALE_STERILIZATION_FOLLOWUP_MILESTONE_1,
                     EC_SCHEDULE_MALE_STERILIZATION_FOLLOWUP_MILESTONE_2));
 
     @Autowired
-    public MaleSterilizationStrategy(ScheduleTrackingService scheduleTrackingService, ActionService actionService) {
+    public MaleSterilizationStrategy(ScheduleTrackingService scheduleTrackingService, ActionService actionService,
+                                     ScheduleService scheduleService) {
         this.scheduleTrackingService = scheduleTrackingService;
         this.actionService = actionService;
+        this.scheduleService = scheduleService;
     }
 
     @Override
@@ -64,8 +65,7 @@ public class MaleSterilizationStrategy implements FPMethodStrategy {
 
     private void enrollECToMaleSterilizationSchedule(String entityId, String referenceDate) {
         logger.info(format("Enrolling EC to Male sterilization Followup schedule. entityId: {0}, Ref date: {1}", entityId, referenceDate));
-        scheduleTrackingService.enroll(new EnrollmentRequest(entityId, maleSterilizationFollowupSchedule.name(), new Time(PREFERED_TIME_FOR_SCHEDULES),
-                parse(referenceDate), null, null, null, null, null));
+        scheduleService.enroll(entityId, maleSterilizationFollowupSchedule.name(), referenceDate);
     }
 
     private void unEnrollECFromMaleSterilizationSchedule(String entityId, String anmId, String submissionDate) {
--- drishti-mother-child/src/test/java/org/ei/drishti/service/scheduling/fpMethodStrategy/MaleSterilizationStrategyTest.java
+++ drishti-mother-child/src/test/java/org/ei/drishti/service/scheduling/fpMethodStrategy/MaleSterilizationStrategyTest.java
@@ -2,6 +2,7 @@ package org.ei.drishti.service.scheduling.fpMethodStrategy;
 
 import org.ei.drishti.domain.FPProductInformation;
 import org.ei.drishti.service.ActionService;
+import org.ei.drishti.service.scheduling.ScheduleService;
 import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,20 +25,22 @@ public class MaleSterilizationStrategyTest {
     private ScheduleTrackingService scheduleTrackingService;
     @Mock
     private ActionService actionService;
+    @Mock
+    private ScheduleService scheduleService;
 
     private MaleSterilizationStrategy strategy;
 
     @Before
     public void setUp() throws Exception {
         initMocks(this);
-        strategy = new MaleSterilizationStrategy(scheduleTrackingService, actionService);
+        strategy = new MaleSterilizationStrategy(scheduleTrackingService, actionService, scheduleService);
     }
 
     @Test
     public void shouldEnrollInMaleSterilizationFollowupScheduleOnECRegistration() throws Exception {
         strategy.registerEC(new FPProductInformation("entity id 1", null, null, null, null, null, null, null, "2012-03-01", "2012-02-01", null, null, null));
 
-        verify(scheduleTrackingService).enroll(enrollmentFor("entity id 1", "Male sterilization Followup", LocalDate.parse("2012-02-01")));
+        verify(scheduleService).enroll("entity id 1", "Male sterilization Followup", "2012-02-01");
     }
 
     @Test
@@ -45,7 +48,7 @@ public class MaleSterilizationStrategyTest {
         strategy.enrollToNewScheduleForNewFPMethod(new FPProductInformation("entity id 1", null, "Male sterilization Followup", "condom",
                 null, null, null, null, "2012-03-01", "2012-02-01", null, null, null));
 
-        verify(scheduleTrackingService).enroll(enrollmentFor("entity id 1", "Male sterilization Followup", LocalDate.parse("2012-02-01")));
+        verify(scheduleService).enroll("entity id 1", "Male sterilization Followup", "2012-02-01");
     }
 
     @Test
