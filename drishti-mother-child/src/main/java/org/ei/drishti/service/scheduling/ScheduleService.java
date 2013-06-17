4364d5af373e04e3b78d417329e8fc8086cb9bcf
Kiran <kirang20@gmail.com>
2013-06-17 10:22:34 +0530

    Harshit & Kiran | Enrollment happens to the right milestone of a schedule based on date. Also added end period in max window for all schedules.

--- drishti-mother-child/src/main/java/org/ei/drishti/service/scheduling/ScheduleService.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/scheduling/ScheduleService.java
@@ -1,27 +1,44 @@
 package org.ei.drishti.service.scheduling;
 
+import org.joda.time.LocalDate;
 import org.motechproject.model.Time;
+import org.motechproject.scheduletracking.api.domain.Milestone;
+import org.motechproject.scheduletracking.api.domain.Schedule;
+import org.motechproject.scheduletracking.api.repository.AllSchedules;
 import org.motechproject.scheduletracking.api.service.EnrollmentRequest;
 import org.motechproject.scheduletracking.api.service.ScheduleTrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static org.ei.drishti.common.util.DateUtil.today;
 import static org.ei.drishti.scheduler.DrishtiScheduleConstants.PREFERED_TIME_FOR_SCHEDULES;
 import static org.joda.time.LocalDate.parse;
 
 @Service
 public class ScheduleService {
     private final ScheduleTrackingService scheduleTrackingService;
+    private final AllSchedules allSchedules;
 
     @Autowired
-    public ScheduleService(ScheduleTrackingService scheduleTrackingService) {
+    public ScheduleService(ScheduleTrackingService scheduleTrackingService, AllSchedules allSchedules) {
         this.scheduleTrackingService = scheduleTrackingService;
+        this.allSchedules = allSchedules;
     }
 
     public void enroll(String entityId, String scheduleName, String referenceDate) {
+        String startingMilestoneName = getStartingMilestoneName(scheduleName, parse(referenceDate));
         EnrollmentRequest request = new EnrollmentRequest(entityId, scheduleName, new Time(PREFERED_TIME_FOR_SCHEDULES),
-                parse(referenceDate), null, null, null, null, null);
+                parse(referenceDate), null, null, null, startingMilestoneName, null);
         scheduleTrackingService.enroll(request);
+    }
+
+    private String getStartingMilestoneName(String name, LocalDate referenceDate) {
+        Schedule schedule = allSchedules.getByName(name);
+        for (Milestone milestone : schedule.getMilestones()) {
+            if (referenceDate.plus(milestone.getMaximumDuration()).isAfter(today()))
+                return milestone.getName();
+        }
+        return null;
     }
 
     public void enroll(String entityId, String scheduleName, String milestone, String referenceDate) {
