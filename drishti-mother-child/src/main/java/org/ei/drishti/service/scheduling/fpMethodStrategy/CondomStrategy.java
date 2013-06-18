a26dca7858c86a2b1eb416161a8abe822ed70a73
Kiran <kirang20@gmail.com>
2013-06-18 11:58:03 +0530

    Kiran | Now an action is created for upcoming reminder as soon as the client is enrolled to Condom refill schedule.

--- drishti-mother-child/src/main/java/org/ei/drishti/service/scheduling/fpMethodStrategy/CondomStrategy.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/scheduling/fpMethodStrategy/CondomStrategy.java
@@ -5,34 +5,42 @@ import org.ei.drishti.domain.FPProductInformation;
 import org.ei.drishti.service.ActionService;
 import org.ei.drishti.service.scheduling.ScheduleService;
 import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.motechproject.scheduletracking.api.service.ScheduleTrackingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static java.text.MessageFormat.format;
 import static java.util.Arrays.asList;
 import static org.ei.drishti.common.util.DateUtil.today;
 import static org.ei.drishti.common.util.IntegerUtil.tryParse;
+import static org.ei.drishti.dto.AlertStatus.upcoming;
+import static org.ei.drishti.dto.BeneficiaryType.ec;
 import static org.ei.drishti.scheduler.DrishtiScheduleConstants.ECSchedulesConstants.EC_SCHEDULE_CONDOM_REFILL;
 import static org.ei.drishti.scheduler.DrishtiScheduleConstants.ECSchedulesConstants.EC_SCHEDULE_CONDOM_REFILL_MILESTONE;
 import static org.joda.time.LocalDate.parse;
 
 @Component
 public class CondomStrategy implements FPMethodStrategy {
+    private static final int DUE_WINDOW_PERIOD_IN_WEEKS = 1;
     private static Logger logger = LoggerFactory.getLogger(CondomStrategy.class.toString());
 
     private final ScheduleTrackingService scheduleTrackingService;
     private final ActionService actionService;
     private final ScheduleService scheduleService;
     private final Schedule condomRefillSchedule = new Schedule(EC_SCHEDULE_CONDOM_REFILL, asList(EC_SCHEDULE_CONDOM_REFILL_MILESTONE));
+    private LocalTime preferredTime;
 
     @Autowired
-    public CondomStrategy(ScheduleTrackingService scheduleTrackingService, ActionService actionService, ScheduleService scheduleService) {
+    public CondomStrategy(ScheduleTrackingService scheduleTrackingService, ActionService actionService, ScheduleService scheduleService,
+                          @Value("#{drishti['preferred.time']}") int preferredTime) {
         this.scheduleTrackingService = scheduleTrackingService;
         this.actionService = actionService;
         this.scheduleService = scheduleService;
+        this.preferredTime = new LocalTime(preferredTime, 0);
     }
 
     @Override
@@ -71,6 +79,8 @@ public class CondomStrategy implements FPMethodStrategy {
     private void enrollECToCondomRefillSchedule(String entityId) {
         logger.info(format("Enrolling EC to Condom Refill schedule. entityId: {0}, Ref date: {1}", entityId, firstDayOfNextMonth()));
         scheduleService.enroll(entityId, condomRefillSchedule.name(), firstDayOfNextMonth().toString());
+        actionService.alertForBeneficiary(ec, entityId, condomRefillSchedule.name(), EC_SCHEDULE_CONDOM_REFILL_MILESTONE, upcoming,
+                firstDayOfNextMonth().toDateTime(preferredTime), firstDayOfNextMonth().plusWeeks(DUE_WINDOW_PERIOD_IN_WEEKS).toDateTime(preferredTime));
     }
 
     private LocalDate firstDayOfNextMonth() {
--- drishti-mother-child/src/test/java/org/ei/drishti/service/scheduling/fpMethodStrategy/CondomStrategyTest.java
+++ drishti-mother-child/src/test/java/org/ei/drishti/service/scheduling/fpMethodStrategy/CondomStrategyTest.java
@@ -3,6 +3,8 @@ package org.ei.drishti.service.scheduling.fpMethodStrategy;
 import org.ei.drishti.domain.FPProductInformation;
 import org.ei.drishti.service.ActionService;
 import org.ei.drishti.service.scheduling.ScheduleService;
+import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
@@ -11,6 +13,8 @@ import org.motechproject.scheduletracking.api.service.ScheduleTrackingService;
 
 import static java.util.Arrays.asList;
 import static org.ei.drishti.common.util.DateUtil.fakeIt;
+import static org.ei.drishti.dto.AlertStatus.upcoming;
+import static org.ei.drishti.dto.BeneficiaryType.ec;
 import static org.joda.time.LocalDate.parse;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -24,11 +28,12 @@ public class CondomStrategyTest {
     private ScheduleService scheduleService;
 
     private CondomStrategy strategy;
+    private static final int preferredTime = 14;
 
     @Before
     public void setUp() throws Exception {
         initMocks(this);
-        strategy = new CondomStrategy(scheduleTrackingService, actionService, scheduleService);
+        strategy = new CondomStrategy(scheduleTrackingService, actionService, scheduleService, preferredTime);
     }
 
     @Test
@@ -38,12 +43,14 @@ public class CondomStrategyTest {
                 , "20", "2012-03-01", null, null, null, null));
 
         verify(scheduleService).enroll("entity id 1", "Condom Refill", "2012-02-01");
+        verify(actionService).alertForBeneficiary(ec, "entity id 1", "Condom Refill", "Condom Refill", upcoming, dateTime("2012-02-01"), dateTime("2012-02-08"));
 
         fakeIt(parse("2012-12-01"));
         strategy.registerEC(new FPProductInformation("entity id 1", "anm id 1", "condom", null, "2012-12-01", null, null
                 , "20", "2012-03-01", null, null, null, null));
 
         verify(scheduleService).enroll("entity id 1", "Condom Refill", "2013-01-01");
+        verify(actionService).alertForBeneficiary(ec, "entity id 1", "Condom Refill", "Condom Refill", upcoming, dateTime("2013-01-01"), dateTime("2013-01-08"));
     }
 
     @Test
@@ -61,6 +68,7 @@ public class CondomStrategyTest {
         strategy.enrollToNewScheduleForNewFPMethod(new FPProductInformation("entity id 1", "anm id 1", "condom", "ocp", null, null, null, null, null, "2012-01-01", null, null, null));
 
         verify(scheduleService).enroll("entity id 1", "Condom Refill", "2012-02-01");
+        verify(actionService).alertForBeneficiary(ec, "entity id 1", "Condom Refill", "Condom Refill", upcoming, dateTime("2012-02-01"), dateTime("2012-02-08"));
     }
 
     @Test
@@ -73,6 +81,7 @@ public class CondomStrategyTest {
         inOrder.verify(scheduleTrackingService).fulfillCurrentMilestone("entity id 1", "Condom Refill", parse("2011-01-12"));
         inOrder.verify(actionService).markAlertAsClosed("entity id 1", "anm id 1", "Condom Refill", "2011-01-12");
         inOrder.verify(scheduleService).enroll("entity id 1", "Condom Refill", "2011-02-01");
+        inOrder.verify(actionService).alertForBeneficiary(ec, "entity id 1", "Condom Refill", "Condom Refill", upcoming, dateTime("2011-02-01"), dateTime("2011-02-08"));
     }
 
     @Test
@@ -81,5 +90,9 @@ public class CondomStrategyTest {
 
         verifyZeroInteractions(scheduleTrackingService);
         verifyZeroInteractions(actionService);
+    }
+
+    private DateTime dateTime(String date) {
+        return parse(date).toDateTime(new LocalTime(preferredTime, 0));
     }
 }
