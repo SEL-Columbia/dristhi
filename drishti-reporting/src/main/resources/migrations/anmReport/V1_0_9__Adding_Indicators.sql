fe07944d94eb65258c36b29c7188c3670ccab7f1
Kumaran V <kumaranv@thoughtworks.com>
2013-10-03 17:59:13 +0530

    Kumaran | #675 | Reporting Measles immunization given when Child Immunizations form is submitted.

--- drishti-reporting/src/main/resources/migrations/anmReport/V1_0_9__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/anmReport/V1_0_9__Adding_Indicators.sql
@@ -64,3 +64,4 @@ INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_OPV_1_1YR');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_OPV_2_1YR');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_OPV_3_1YR');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_HEPB_0_1YR');
+INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_MEASLES_1YR');
