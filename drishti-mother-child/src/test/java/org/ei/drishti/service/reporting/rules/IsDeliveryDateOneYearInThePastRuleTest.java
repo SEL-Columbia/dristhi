2b45cd61e76cc1eadaa7b3398530aa70eb1130e4
Kumaran V <kumaranv@thoughtworks.com>
2013-10-08 10:37:29 +0530

    Kumaran | #645 | Checking the age of child with Delivery Date instead of dateOfBirth field for immunisation indicators in PNC_Registration_OA form  submission.

---
+++ drishti-mother-child/src/test/java/org/ei/drishti/service/reporting/rules/IsDeliveryDateOneYearInThePastRuleTest.java
@@ -0,0 +1,40 @@
+package org.ei.drishti.service.reporting.rules;
+
+import org.ei.drishti.util.SafeMap;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class IsDeliveryDateOneYearInThePastRuleTest {
+
+    private IsDeliveryDateOneYearInThePastRule rule;
+
+    @Before
+    public void setUp() throws Exception {
+        rule = new IsDeliveryDateOneYearInThePastRule();
+    }
+
+    @Test
+    public void shouldReturnTrueIfDeliveryDateIsLessThanOneYear() {
+        SafeMap safeMap = new SafeMap();
+        safeMap.put("serviceProvidedDate", "2012-03-01");
+        safeMap.put("referenceDate", "2012-01-01");
+
+        boolean didRuleApply = rule.apply(safeMap);
+
+        assertTrue(didRuleApply);
+    }
+
+    @Test
+    public void shouldReturnTrueIfDeliveryDateIsNotLessThanOneYear() {
+        SafeMap safeMap = new SafeMap();
+        safeMap.put("serviceProvidedDate", "2012-03-01");
+        safeMap.put("referenceDate", "2011-01-01");
+
+        boolean didRuleApply = rule.apply(safeMap);
+
+        assertFalse(didRuleApply);
+    }
+}
