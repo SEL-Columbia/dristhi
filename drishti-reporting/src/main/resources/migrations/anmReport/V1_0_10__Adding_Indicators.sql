91478c35b43de0cff5709c880c648d4043ef4b8f
Kiran <kirang20@gmail.com>
2013-10-10 18:01:42 +0530

    Kiran | #626 | Inserted IFA indicator into report db and to Indicator enum.

--- drishti-reporting/src/main/resources/migrations/anmReport/V1_0_10__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/anmReport/V1_0_10__Adding_Indicators.sql
@@ -1,2 +1,3 @@
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('M_LIVE_BIRTH');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('F_LIVE_BIRTH');
+INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_IFA_100');
--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_14__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_14__Adding_Indicators.sql
@@ -1,2 +1,3 @@
 INSERT INTO report.dim_indicator (indicator) VALUES ('M_LIVE_BIRTH');
 INSERT INTO report.dim_indicator (indicator) VALUES ('F_LIVE_BIRTH');
+INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_IFA_100');
