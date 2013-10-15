3bc8bf74c0ea8e65810a069fbc62e90350ee3246
Kumaran V <kumaranv@thoughtworks.com>
2013-10-15 15:50:10 +0530

    Kumaran | #714 | Reporting children more than 5 years of age immunised with DPT booster 2 during the reporting month at Sub_Center.

--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_14__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_14__Adding_Indicators.sql
@@ -4,3 +4,4 @@ INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_IFA_100');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_IMMUNIZATION_DEATH');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_5_3YR');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_9_3YR');
+INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_DPTBOOSTER_2_>5YR');
