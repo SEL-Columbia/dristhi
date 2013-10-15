1be2454e531478a69b6cff7977e15dfadca40773
Kumaran V <kumaranv@thoughtworks.com>
2013-10-15 15:19:09 +0530

    Kumaran | #713 | Reporting children under 3 years given Vit-A 9th dose in sub-Centre during the reporting month.

--- drishti-reporting/src/main/resources/migrations/anmReport/V1_0_10__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/anmReport/V1_0_10__Adding_Indicators.sql
@@ -3,3 +3,4 @@ INSERT INTO anm_report.dim_indicator (indicator) VALUES ('F_LIVE_BIRTH');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_IFA_100');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_IMMUNIZATION_DEATH');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_5_3YR');
+INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_9_3YR');
--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_14__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_14__Adding_Indicators.sql
@@ -3,3 +3,4 @@ INSERT INTO report.dim_indicator (indicator) VALUES ('F_LIVE_BIRTH');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_IFA_100');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_IMMUNIZATION_DEATH');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_5_3YR');
+INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_9_3YR');
