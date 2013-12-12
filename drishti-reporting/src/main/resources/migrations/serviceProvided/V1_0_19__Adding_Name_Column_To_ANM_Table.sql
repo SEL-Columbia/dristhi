76421a6d40d582cb6126b795c0b4807b992bef8c
Thirunavukkarasu <thirunar@thoughtworks.com>
2013-12-12 16:15:29 +0530

    Thiru & Kiran | #759 | Added name column to report.dim_anm table

---
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_19__Adding_Name_Column_To_ANM_Table.sql
@@ -0,0 +1,51 @@
+ALTER TABLE report.dim_anm ADD COLUMN name varchar(100);
+
+UPDATE report.dim_anm SET name='Elizebeth D''souza' WHERE anmidentifier='bhe1';
+
+UPDATE report.dim_anm SET name='Jyothilakshmi' WHERE anmidentifier='bhe2';
+
+UPDATE report.dim_anm SET name='Jayanthi' WHERE anmidentifier='bhe3';
+
+UPDATE report.dim_anm SET name='Hemalatha' WHERE anmidentifier='bhe4';
+
+UPDATE report.dim_anm SET name='Latha' WHERE anmidentifier='bhe5';
+
+UPDATE report.dim_anm SET name='Hemavathi' WHERE anmidentifier='klp4';
+
+UPDATE report.dim_anm SET name='N.R. Kamalakshi' WHERE anmidentifier='klp2';
+
+UPDATE report.dim_anm SET name='Dhakshakanya' WHERE anmidentifier='klp1';
+
+UPDATE report.dim_anm SET name='B.T. Indira' WHERE anmidentifier='klp3';
+
+UPDATE report.dim_anm SET name='C' WHERE anmidentifier='c';
+
+UPDATE report.dim_anm SET name='Demo1' WHERE anmidentifier='demo1';
+
+UPDATE report.dim_anm SET name='Demo2' WHERE anmidentifier='demo2';
+
+UPDATE report.dim_anm SET name='Demo3' WHERE anmidentifier='demo3';
+
+UPDATE report.dim_anm SET name='Trainer1' WHERE anmidentifier='trainer1';
+
+UPDATE report.dim_anm SET name='Trainer2' WHERE anmidentifier='trainer2';
+
+UPDATE report.dim_anm SET name='User1' WHERE anmidentifier='user1';
+
+UPDATE report.dim_anm SET name='User2' WHERE anmidentifier='user2';
+
+UPDATE report.dim_anm SET name='User3' WHERE anmidentifier='user3';
+
+UPDATE report.dim_anm SET name='User4' WHERE anmidentifier='user4';
+
+UPDATE report.dim_anm SET name='User5' WHERE anmidentifier='user5';
+
+UPDATE report.dim_anm SET name='User6' WHERE anmidentifier='user6';
+
+UPDATE report.dim_anm SET name='User7' WHERE anmidentifier='user7';
+
+UPDATE report.dim_anm SET name='User8' WHERE anmidentifier='user8';
+
+UPDATE report.dim_anm SET name='User9' WHERE anmidentifier='user9';
+
+UPDATE report.dim_anm SET name='User10' WHERE anmidentifier='user10';
