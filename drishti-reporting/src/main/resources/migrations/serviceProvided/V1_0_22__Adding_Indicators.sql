214bf389814cf8ec2a7050e8abd32d490738f901
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-02-18 14:55:09 +0530

    Thiru | #868 | Created test users and moved few indicators to new delta

--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_22__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_22__Adding_Indicators.sql
@@ -1,5 +1,2 @@
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_LIVE_BIRTH');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_STILL_BIRTH');
-INSERT INTO report.dim_indicator (indicator) VALUES ('OPV3');
-INSERT INTO report.dim_indicator (indicator) VALUES ('OPV_BOOSTER');
-INSERT INTO report.dim_indicator (indicator) VALUES ('DPT_BOOSTER_1');
