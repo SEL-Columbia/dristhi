ba819c96e8f4729331957bbb0a5e7b94bb75448f
Kumaran Venkataraman <me@kumaranvram.com>
2014-02-20 11:45:23 +0530

    Kumaran | #769 | Added CORS Headers for authentication URL.

--- drishti-web/src/main/java/org/ei/drishti/web/controller/UserController.java
+++ drishti-web/src/main/java/org/ei/drishti/web/controller/UserController.java
@@ -1,17 +1,28 @@
 package org.ei.drishti.web.controller;
 
+import org.ei.drishti.web.HttpHeaderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import static org.ei.drishti.web.HttpHeaderFactory.allowOrigin;
 import static org.springframework.http.HttpStatus.OK;
 
 @Controller
 public class UserController {
+    private String drishtiSiteUrl;
+
+    @Autowired
+    public UserController(@Value("#{drishti['drishti.site.url']}") String drishtiSiteUrl) {
+        this.drishtiSiteUrl = drishtiSiteUrl;
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "/authenticate-user")
     public ResponseEntity<HttpStatus> authenticateUser() {
-        return new ResponseEntity<>(OK);
+        return new ResponseEntity<>(null, allowOrigin(drishtiSiteUrl), OK);
     }
 }
