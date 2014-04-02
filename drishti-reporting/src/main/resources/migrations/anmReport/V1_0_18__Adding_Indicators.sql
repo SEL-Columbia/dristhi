276b054b476e541e985325bdf88c8faf9b51b7e2
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-04-02 12:19:54 +0530

    Thiru | #940 | Added NRHM_VIT_A_1 indicator

--- drishti-reporting/src/main/resources/migrations/anmReport/V1_0_18__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/anmReport/V1_0_18__Adding_Indicators.sql
@@ -4,3 +4,4 @@ INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_TT1');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_SUB_TT');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_BF_POST_BIRTH');
 INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_SPONTANEOUS_ABORTION');
+INSERT INTO anm_report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_1');
--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_26__Adding_Indicators.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_26__Adding_Indicators.sql
@@ -4,3 +4,4 @@ INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_TT1');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_SUB_TT');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_BF_POST_BIRTH');
 INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_SPONTANEOUS_ABORTION');
+INSERT INTO report.dim_indicator (indicator) VALUES ('NRHM_VIT_A_1');
