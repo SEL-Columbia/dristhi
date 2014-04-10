c69af8c4834e4e13fbc6b7e78d4b81ff0fc1a1dc
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-04-10 15:54:32 +0530

    Thiru | #940 | Fixed an issue with  NRHM_ANC_LT_12_weeks indicator.

--- drishti-mother-child/src/main/java/org/ei/drishti/service/reporting/rules/IsANCRegisteredWithinTwelveWeeksOfPregnancy.java
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/reporting/rules/IsANCRegisteredWithinTwelveWeeksOfPregnancy.java
@@ -3,8 +3,8 @@ package org.ei.drishti.service.reporting.rules;
 import org.ei.drishti.util.SafeMap;
 import org.springframework.stereotype.Component;
 
-import static org.ei.drishti.common.AllConstants.ANCFormFields.REGISTRATION_DATE;
 import static org.ei.drishti.common.AllConstants.CommonFormFields.REFERENCE_DATE;
+import static org.ei.drishti.common.AllConstants.CommonFormFields.SERVICE_PROVIDED_DATE;
 import static org.joda.time.LocalDate.parse;
 
 @Component
@@ -14,7 +14,7 @@ public class IsANCRegisteredWithinTwelveWeeksOfPregnancy implements IRule {
     @Override
     public boolean apply(SafeMap reportFields) {
 
-        return !(parse(reportFields.get(REGISTRATION_DATE)).minusDays(NUMBER_OF_DAYS_IN_12_WEEKS)
+        return !(parse(reportFields.get(SERVICE_PROVIDED_DATE)).minusDays(NUMBER_OF_DAYS_IN_12_WEEKS)
                 .isAfter(parse(reportFields.get(REFERENCE_DATE))));
     }
 }
--- drishti-mother-child/src/test/java/org/ei/drishti/service/reporting/rules/IsANCRegisteredWithinTwelveWeeksOfPregnancyTest.java
+++ drishti-mother-child/src/test/java/org/ei/drishti/service/reporting/rules/IsANCRegisteredWithinTwelveWeeksOfPregnancyTest.java
@@ -22,14 +22,14 @@ public class IsANCRegisteredWithinTwelveWeeksOfPregnancyTest {
 
     @Test
     public void shouldReturnTrueWhenANCIsRegisteredWithin12Weeks() {
-        Map<String, String> reportData = create("registrationDate", "2012-10-23")
+        Map<String, String> reportData = create("serviceProvidedDate", "2012-10-23")
                 .put("referenceDate", "2012-10-01")
                 .map();
         boolean didRuleSucceed = rule.apply(new SafeMap(reportData));
 
         assertTrue(didRuleSucceed);
 
-        reportData = create("registrationDate", "2013-03-26")
+        reportData = create("serviceProvidedDate", "2013-03-26")
                 .put("referenceDate", "2013-01-01")
                 .map();
         didRuleSucceed = rule.apply(new SafeMap(reportData));
@@ -39,14 +39,14 @@ public class IsANCRegisteredWithinTwelveWeeksOfPregnancyTest {
 
     @Test
     public void shouldReturnFalseWhenANCIsRegisteredAfter12Weeks() {
-        Map<String, String> reportData = create("registrationDate", "2013-10-23")
+        Map<String, String> reportData = create("serviceProvidedDate", "2013-10-23")
                 .put("referenceDate", "2012-10-01")
                 .map();
         boolean didRuleSucceed = rule.apply(new SafeMap(reportData));
 
         assertFalse(didRuleSucceed);
 
-        reportData = create("registrationDate", "2013-03-27")
+        reportData = create("serviceProvidedDate", "2013-03-27")
                 .put("referenceDate", "2013-01-01")
                 .map();
         didRuleSucceed = rule.apply(new SafeMap(reportData));
