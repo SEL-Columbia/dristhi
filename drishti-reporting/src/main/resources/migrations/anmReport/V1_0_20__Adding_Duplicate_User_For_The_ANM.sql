26f0b234fe42f341303f726665bc308031459b43
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-05-23 10:32:18 +0530

    Thiru | #997 | Created another user for bud1 ANM.

--- drishti-reporting/src/main/resources/migrations/anmReport/V1_0_20__Adding_Duplicate_User_For_The_ANM.sql
+++ drishti-reporting/src/main/resources/migrations/anmReport/V1_0_20__Adding_Duplicate_User_For_The_ANM.sql
@@ -1,1 +1,3 @@
 INSERT INTO anm_report.dim_anm (anmIdentifier)  SELECT 'srir1' WHERE NOT EXISTS (SELECT dim_anm FROM anm_report.dim_anm WHERE anmIdentifier='srir1');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier)  SELECT 'budg1' WHERE NOT EXISTS (SELECT dim_anm FROM anm_report.dim_anm WHERE anmIdentifier='budg1');
