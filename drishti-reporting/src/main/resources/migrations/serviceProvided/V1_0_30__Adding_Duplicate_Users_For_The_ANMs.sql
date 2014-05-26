60f1b6dbc02d2f49f59657443a489581b961ccd2
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-05-26 12:30:05 +0530

    Thiru | #1001 | Created another user for kara1.

--- drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_30__Adding_Duplicate_Users_For_The_ANMs.sql
+++ drishti-reporting/src/main/resources/migrations/serviceProvided/V1_0_30__Adding_Duplicate_Users_For_The_ANMs.sql
@@ -5,3 +5,7 @@ INSERT INTO report.dim_service_provider (service_provider, type) SELECT (SELECT 
 INSERT INTO report.dim_anm (anmIdentifier, phc, name, subcenter)  SELECT 'budg1', (SELECT ID FROM report.dim_phc WHERE phcIdentifier='phc_budgumpa'), 'K.N.Bhairamatti', 'budgumpa' WHERE NOT EXISTS (SELECT dim_anm FROM report.dim_anm WHERE anmIdentifier='budg1');
 
 INSERT INTO report.dim_service_provider (service_provider, type) SELECT (SELECT ID FROM report.dim_anm WHERE anmIdentifier='budg1'), (SELECT ID FROM report.dim_service_provider_type WHERE type='ANM') WHERE NOT EXISTS (SELECT service_provider FROM report.dim_service_provider WHERE service_provider=(SELECT ID FROM report.dim_anm WHERE anmIdentifier='budg1'));
+
+INSERT INTO report.dim_anm (anmIdentifier, phc, name, subcenter)  SELECT 'karat1', (SELECT ID FROM report.dim_phc WHERE phcIdentifier='chc_karatagi'), 'Bhagyalakshmi', 'karatagi_b' WHERE NOT EXISTS (SELECT dim_anm FROM report.dim_anm WHERE anmIdentifier='karat1');
+
+INSERT INTO report.dim_service_provider (service_provider, type) SELECT (SELECT ID FROM report.dim_anm WHERE anmIdentifier='karat1'), (SELECT ID FROM report.dim_service_provider_type WHERE type='ANM') WHERE NOT EXISTS (SELECT service_provider FROM report.dim_service_provider WHERE service_provider=(SELECT ID FROM report.dim_anm WHERE anmIdentifier='karat1'));
