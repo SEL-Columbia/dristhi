f0f1c4bdffe411be4d99da82c2c29fedd0e3fb24
Kumaran Venkataraman <me@kumaranvram.com>
2014-06-11 16:02:49 +0530

    Kumaran | #1013 | Added 10 more users for last batch of training

---
+++ drishti-reporting/src/main/resources/migrations/anmReport/V1_0_23__Adding_Ten_More_User_Data_For_Last_Batch_Training.sql
@@ -0,0 +1,29 @@
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('trainer11');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('trainer12');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('trainer13');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('trainer14');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('trainer15');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user41');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user42');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user43');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user44');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user45');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user46');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user47');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user48');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user49');
+
+INSERT INTO anm_report.dim_anm (anmIdentifier) VALUES ('user50');
