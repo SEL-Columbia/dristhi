d0615d48659a2fdb60eef442e2c151e1a65303fc
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-06-23 11:32:34 +0530

    Thiru | #1009 | Added PPC I, PPC S, PPC J MCTS indicators.

---
+++ drishti-mother-child/src/main/java/org/ei/drishti/service/reporting/rules/CurrentFPMethodIsDMPAInjectableRule.java
@@ -0,0 +1,16 @@
+package org.ei.drishti.service.reporting.rules;
+
+import org.ei.drishti.util.SafeMap;
+import org.springframework.stereotype.Component;
+
+import static org.ei.drishti.common.AllConstants.FamilyPlanningFormFields.CURRENT_FP_METHOD_FIELD_NAME;
+import static org.ei.drishti.common.AllConstants.FamilyPlanningFormFields.DMPA_INJECTABLE_FP_METHOD_VALUE;
+
+@Component
+public class CurrentFPMethodIsDMPAInjectableRule implements IRule {
+
+    @Override
+    public boolean apply(SafeMap reportFields) {
+        return DMPA_INJECTABLE_FP_METHOD_VALUE.equalsIgnoreCase(reportFields.get(CURRENT_FP_METHOD_FIELD_NAME));
+    }
+}
