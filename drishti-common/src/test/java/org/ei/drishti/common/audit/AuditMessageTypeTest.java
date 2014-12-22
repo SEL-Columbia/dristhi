8751b4a5f6cc51f5b95c086eb49c2dc5bc6e4733
Glauco Vinicius <gl4uc0@gmail.com>
2014-12-22 13:57:15 -0200

    Added more test scenarios around AuditMessageType class

--- drishti-common/src/test/java/org/ei/drishti/common/audit/AuditMessageTypeTest.java
+++ drishti-common/src/test/java/org/ei/drishti/common/audit/AuditMessageTypeTest.java
@@ -1,7 +1,9 @@
 package org.ei.drishti.common.audit;
 
 import org.junit.Test;
+import sun.text.resources.sr.FormatData_sr_BA;
 
+import static org.ei.drishti.common.audit.AuditMessageType.FORM_SUBMISSION;
 import static org.ei.drishti.common.audit.AuditMessageType.NORMAL;
 import static org.ei.drishti.common.audit.AuditMessageType.SMS;
 import static org.hamcrest.core.Is.is;
@@ -9,11 +11,24 @@ import static org.junit.Assert.assertThat;
 
 public class AuditMessageTypeTest {
     @Test
-    public void shouldKnowWhichFieldsAreSupportedByAGivenTypeOfMessage() {
-        assertThat(SMS.supports("message"), is(true));
-        assertThat(SMS.supports("someRandomField"), is(false));
-
+    public void shouldKnowWhichFieldsAreSupportedByNornalTypeOfMessage() {
         assertThat(NORMAL.supports("data"), is(true));
         assertThat(NORMAL.supports("someOtherField"), is(false));
+    }
+
+    @Test
+    public void shouldKnowWhichFieldsAreSupportedByFormSubmissionTypeOfMessage() {
+        assertThat(FORM_SUBMISSION.supports("formId"), is(true));
+        assertThat(FORM_SUBMISSION.supports("formType"), is(true));
+        assertThat(FORM_SUBMISSION.supports("formData"), is(true));
+        assertThat(FORM_SUBMISSION.supports("someRandomField"), is(false));
+    }
+
+    @Test
+    public void shouldKnowWhichFieldsAreSupportedBySmsTypeOfMessage() {
+        assertThat(SMS.supports("recipient"), is(true));
+        assertThat(SMS.supports("message"), is(true));
+        assertThat(SMS.supports("smsIsSent"), is(true));
+        assertThat(SMS.supports("someRandomField"), is(false));
     }
 }
