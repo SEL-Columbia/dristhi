35ac0236da05eacf90dce9e5a7b602886ddc5166
kercobo <mib.iqbaal@gmail.com>
2015-02-12 14:12:13 +0800

    adding person entity

---
+++ drishti-mother-child/src/main/java/org/ei/drishti/domain/Person.java
@@ -0,0 +1,118 @@
+package org.ei.drishti.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.ektorp.support.TypeDiscriminator;
+import org.motechproject.model.MotechBaseDataObject;
+
+@TypeDiscriminator("doc.type === 'Person'")
+public class Person extends MotechBaseDataObject {
+	@JsonProperty
+	private String caseId;
+	@JsonProperty
+	private String tbId;
+	@JsonProperty
+	private String name;
+	@JsonProperty
+	private String dob;
+	@JsonProperty
+	private String gender;
+	@JsonProperty
+	private String address;
+	@JsonProperty
+	private String contactNumber;
+	@JsonProperty
+	private Integer height;
+	@JsonProperty
+	private String patientType;
+
+	public String getCaseId() {
+		return caseId;
+	}
+
+	public void setCaseId(String caseId) {
+		this.caseId = caseId;
+	}
+
+	public String getTbId() {
+		return tbId;
+	}
+
+	public void setTbId(String tbId) {
+		this.tbId = tbId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+	public String getPatientType() {
+		return patientType;
+	}
+
+	public void setPatientType(String patientType) {
+		this.patientType = patientType;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o, "id", "revision");
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this, "id", "revision");
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+}
