f4d445d7a3d13427ca8e442097a6e1ca57964b6c
Julkar Nain <julkar@mpower-social.com>
2015-02-28 10:17:45 +0600

    remove drishti-common module and now the new name is opensrp-common

--- drishti-common/src/main/java/org/opensrp/common/audit/ForbiddenFieldInAuditMessage.java
+++
@@ -1,9 +0,0 @@
-package org.ei.drishti.common.audit;
-
-import static java.text.MessageFormat.format;
-
-public class ForbiddenFieldInAuditMessage extends RuntimeException {
-    public ForbiddenFieldInAuditMessage(AuditMessageType type, String key, String value) {
-        super(format("Not allowed to add field: {0} => {1} for audit message of type: {2}", key, value, type));
-    }
-}
