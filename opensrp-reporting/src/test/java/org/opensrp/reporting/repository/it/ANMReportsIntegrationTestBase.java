d0ad2e224240b4ba729d446d4856e83cc8d1c26e
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-03-03 13:13:10 +0500

    MK: ApplicationContext file name should not contain drishti

--- opensrp-reporting/src/test/java/org/opensrp/reporting/repository/it/ANMReportsIntegrationTestBase.java
+++ opensrp-reporting/src/test/java/org/opensrp/reporting/repository/it/ANMReportsIntegrationTestBase.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring/applicationContext-drishti-reporting-test.xml")
+@ContextConfiguration("classpath:spring/applicationContext-opensrp-reporting-test.xml")
 public class ANMReportsIntegrationTestBase {
     @Autowired
     protected TestANMReportDataAccessTemplate template;
