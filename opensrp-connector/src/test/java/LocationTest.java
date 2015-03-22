ec2105650bb6bcf16a6d9562f7f02d387ac17a66
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-03-22 23:19:57 +0500

    MK: added Gson parsing test

--- opensrp-connector/src/test/java/LocationTest.java
+++ opensrp-connector/src/test/java/LocationTest.java
@@ -4,6 +4,8 @@ import org.opensrp.api.domain.Location;
 import org.opensrp.api.util.LocationTree;
 import org.opensrp.connector.openmrs.LocationService;
 
+import com.google.gson.Gson;
+
 
 public class LocationTest {
 	String openmrsOpenmrsUrl = "http://46.101.51.199:8080/openmrs/";
@@ -21,13 +23,22 @@ public class LocationTest {
 	public void testLocationTree() throws JSONException {
 		LocationService ls = new LocationService(openmrsOpenmrsUrl, openmrsUsername, openmrsPassword);
 		LocationTree l = ls.getLocationTree();
-		System.out.println(l);
+		String tj = new Gson().toJson(l, LocationTree.class);
+		System.out.println(tj);
+		LocationTree l2 = new Gson().fromJson(tj, LocationTree.class);
+		String tj1 = new Gson().toJson(l2, LocationTree.class);
+		System.out.println(tj1);
 	}
 	
 	@Test
 	public void testLocationTreeOf() throws JSONException {
 		LocationService ls = new LocationService(openmrsOpenmrsUrl, openmrsUsername, openmrsPassword);
+		System.out.println("/////////////////////testloc");
 		LocationTree l = ls.getLocationTreeOf("testloc");
-		System.out.println(l);
+		String tj = new Gson().toJson(l, LocationTree.class);
+		System.out.println(tj);
+		LocationTree l2 = new Gson().fromJson(tj, LocationTree.class);
+		String tj1 = new Gson().toJson(l2, LocationTree.class);
+		System.out.println(tj1);
 	}
 }
