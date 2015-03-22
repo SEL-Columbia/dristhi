88b1c0654f822d94f50dee4431d6951b3197d340
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-03-22 10:19:40 +0500

    MK: controllers to provide interface to client app for access to access control and location management services

---
+++ opensrp-web/src/main/java/org/opensrp/web/controller/LocationController.java
@@ -0,0 +1,36 @@
+package org.opensrp.web.controller;
+
+import org.json.JSONException;
+import org.opensrp.api.util.LocationTree;
+import org.opensrp.connector.openmrs.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/location/")
+public class LocationController {
+	
+	private LocationService openmrsLocationService;
+	
+	@Autowired
+	public LocationController(LocationService openmrsLocationService) {
+		this.openmrsLocationService = openmrsLocationService;
+	}
+	
+	@RequestMapping("location-tree")
+	@ResponseBody
+	public ResponseEntity<LocationTree> getLocationTree() throws JSONException {
+		return new ResponseEntity<>(openmrsLocationService.getLocationTree(),HttpStatus.OK);
+	}
+	
+	@RequestMapping("location-tree/{uuid}")
+	@ResponseBody
+	public ResponseEntity<LocationTree> getLocationTree(@PathVariable("uuid") String uuid) throws JSONException {
+		return new ResponseEntity<>(openmrsLocationService.getLocationTreeOf(uuid),HttpStatus.OK);
+	}
+}
