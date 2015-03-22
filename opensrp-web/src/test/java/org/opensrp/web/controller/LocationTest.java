3d14eedd7f36d3f850d0355bb2a8e904d925639c
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-03-22 10:21:36 +0500

    MK: test classes for Location and Access control

---
+++ opensrp-web/src/test/java/org/opensrp/web/controller/LocationTest.java
@@ -0,0 +1,41 @@
+package org.opensrp.web.controller;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.opensrp.api.util.LocationTree;
+import org.opensrp.connector.openmrs.LocationService;
+import org.springframework.http.ResponseEntity;
+
+public class LocationTest {
+
+	private LocationService locationservice;
+	private LocationController controller;
+	private String openmrsOpenmrsUrl = "http://46.101.51.199:8080/openmrs/";
+	private String openmrsUsername = "admin";
+	private String openmrsPassword = "5rpAdmin";
+	
+	@Before
+    public void setUp() throws Exception {
+		this.locationservice = new LocationService(openmrsOpenmrsUrl, openmrsUsername, openmrsPassword);
+        this.controller = new LocationController(locationservice);
+	}
+	
+	@Test
+	public void testLocationTree() throws JSONException{
+		ResponseEntity<LocationTree> l = controller.getLocationTree();
+		assertNotNull(l.getBody().findLocation("60c21502-fec1-40f5-b77d-6df3f92771ce"));		
+		assertTrue(l.getBody().findLocation("60c21502-fec1-40f5-b77d-6df3f92771ce").getName().equalsIgnoreCase("testloc4"));
+	}
+	
+	@Test
+	public void testLocationTreeWithId() throws JSONException{
+		ResponseEntity<LocationTree> l = controller.getLocationTree("60c21502-fec1-40f5-b77d-6df3f92771ce");
+		assertNotNull(l.getBody().findLocation("60c21502-fec1-40f5-b77d-6df3f92771ce"));		
+		assertTrue(l.getBody().findLocation("60c21502-fec1-40f5-b77d-6df3f92771ce").getName().equalsIgnoreCase("testloc4"));
+	}
+	
+}
---
+++ opensrp-web/src/test/java/org/opensrp/web/controller/SecurityManagerTest.java
@@ -0,0 +1,40 @@
+package org.opensrp.web.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.opensrp.api.domain.Location;
+import org.opensrp.api.domain.User;
+import org.opensrp.connector.openmrs.LocationService;
+import org.opensrp.connector.openmrs.UserService;
+import org.springframework.http.ResponseEntity;
+
+public class SecurityManagerTest {
+
+	private UserService userservice;
+	private LocationService locationservice;
+
+	private SecurityManagerController securityManager;
+	private String openmrsOpenmrsUrl = "http://46.101.51.199:8080/openmrs/";
+	private String openmrsUsername = "admin";
+	private String openmrsPassword = "5rpAdmin";
+	
+	@Before
+    public void setUp() throws Exception {
+		this.userservice = new UserService(openmrsOpenmrsUrl, openmrsUsername, openmrsPassword);
+		this.locationservice = new LocationService(openmrsOpenmrsUrl, openmrsUsername, openmrsPassword);
+
+        this.securityManager = new SecurityManagerController(userservice, locationservice);
+	}
+	
+	@Test
+	public void testAuthentication() throws JSONException{
+		ResponseEntity<Map<String, Object>> m = securityManager.authenticate("admin", "5rpAdmin");
+		assertTrue(((User)m.getBody().get("user")).getUsername().equalsIgnoreCase("admin"));
+		assertTrue(((Location)m.getBody().get("location")).getName().equalsIgnoreCase("testloc"));
+	}
+}
