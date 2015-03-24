3bb015b08cd3916ece538b92ec98fb6fbfe41bf8
Julkar Nain <julkar@mpower-social.com>
2015-03-24 20:12:08 +0600

    Julkar : Have added a package for Integration test where the I write the test classes  AllClientsTest, AllProvidersTest, AllUsersTest, AllLocationsTest, AllEventsTest, AllBaseEntitiesTest classes and added in the test service dir BaseEntityServiceTest

---
+++ opensrp-core/src/test/java/org/opensrp/repository/it/AllClientsIntegrationTest.java
@@ -0,0 +1,61 @@
+package org.opensrp.repository.it;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opensrp.repository.AllClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-applicationContext-opensrp.xml")
+public class AllClientsIntegrationTest {
+
+	
+	@Autowired
+	private AllClients allClients;
+
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+
+	@Test
+	public void shouldAddClient()
+	{
+		/*Map<String, String> identifiers = new HashMap<String, String>();
+		identifiers.put("identifier-1",
+				"FWA-1");
+
+		
+		org.opensrp.domain.Client domainClient = new org.opensrp.domain.Client()
+														.withBaseEntityId("0001")
+														.withIdentifiers(identifiers);
+		
+		allClients.add(domainClient);*/
+		
+		org.opensrp.domain.Client getClientByBaseEntityId = allClients
+				.findByBaseEntityId("0001");
+		
+		assertThat(getClientByBaseEntityId.getBaseEntityId(), is("0001"));
+	}
+	
+	/*@Test
+	public void shouldGetClientbyIdentifiers()
+	{
+		org.opensrp.domain.Client getClientByKeyId = allClients
+				.findClientByIds("id2");
+		
+		assertThat(getClientByKeyId.getBaseEntityId(), is("0001"));
+		
+	}*/
+	
+}
---
+++ opensrp-core/src/test/java/org/opensrp/repository/it/AllProvidersIntegrationTest.java
@@ -0,0 +1,56 @@
+package org.opensrp.repository.it;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opensrp.repository.AllProviders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-applicationContext-opensrp.xml")
+public class AllProvidersIntegrationTest {
+	
+	@Autowired
+	private AllProviders allProviders;
+
+	@Before
+	public void setUp() throws Exception {
+		initMocks(this);
+	}
+
+	@Test
+	public void shouldAddProvider()
+	{
+		/*Map<String, String> identifiers = new HashMap<String, String>();
+		identifiers.put("identifier-1",
+				"FWA-1");
+
+		
+		org.opensrp.domain.Provider domainProvider = new org.opensrp.domain.Provider()
+														.withBaseEntityId("0001")
+														.withIdentifiers(identifiers);
+		
+		
+		allProviders.add(domainProvider);*/
+		
+		
+		
+		org.opensrp.domain.Provider baseEntityIdInProvider = allProviders
+				.findByBaseEntityId("0001");
+
+		//assertThat(allBaseEntitiesInDB, is(asList(domainBaseEntity)));
+		assertThat(baseEntityIdInProvider.getBaseEntityId(), is("0001"));
+		
+		
+	}
+}
