7d3cc303082001081c8407fc8e59e11818c4879d
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-04-02 14:58:05 +0500

    MK: hooking into the formSubmission submit by device to make sure that it submits to openmrs as soon as received

--- opensrp-web/src/main/java/org/opensrp/web/controller/FormSubmissionController.java
+++ opensrp-web/src/main/java/org/opensrp/web/controller/FormSubmissionController.java
@@ -1,11 +1,18 @@
 package org.opensrp.web.controller;
 
 import ch.lambdaj.function.convert.Converter;
+
 import org.opensrp.event.FormSubmissionEvent;
 import org.opensrp.form.domain.FormSubmission;
 import org.opensrp.form.service.FormSubmissionConverter;
 import org.opensrp.form.service.FormSubmissionService;
+import org.json.JSONObject;
 import org.motechproject.scheduler.gateway.OutboundEventGateway;
+import org.opensrp.api.domain.Client;
+import org.opensrp.api.domain.Event;
+import org.opensrp.connector.OpenmrsConnector;
+import org.opensrp.connector.openmrs.service.EncounterService;
+import org.opensrp.connector.openmrs.service.PatientService;
 import org.opensrp.dto.form.FormSubmissionDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.List;
 
@@ -31,11 +41,20 @@ public class FormSubmissionController {
     private static Logger logger = LoggerFactory.getLogger(FormSubmissionController.class.toString());
     private FormSubmissionService formSubmissionService;
     private OutboundEventGateway gateway;
+    
+    private EncounterService encounterService;
+    private OpenmrsConnector openmrsConnector;
+    private PatientService patientService;
 
     @Autowired
-    public FormSubmissionController(FormSubmissionService formSubmissionService, OutboundEventGateway gateway) {
+    public FormSubmissionController(FormSubmissionService formSubmissionService, OutboundEventGateway gateway,
+    		EncounterService encounterService, OpenmrsConnector openmrsConnector, PatientService patientService) {
         this.formSubmissionService = formSubmissionService;
         this.gateway = gateway;
+        
+        this.encounterService = encounterService;
+        this.openmrsConnector = openmrsConnector;
+        this.patientService = patientService;
     }
 
     @RequestMapping(method = GET, value = "/form-submissions")
@@ -77,6 +96,35 @@ public class FormSubmissionController {
             }
 
             gateway.sendEventMessage(new FormSubmissionEvent(formSubmissionsDTO).toEvent());
+            
+            try{
+            ////////TODO MAIMOONA : SHOULD BE IN EVENT but event needs to be moved to web so for now kept here
+            String json = new Gson().toJson(formSubmissionsDTO);
+            System.out.println("MMMMMMMMMMMYYYYYYYYYYYYYY::"+json);
+            List<FormSubmissionDTO> formSubmissions = new Gson().fromJson(json, new TypeToken<List<FormSubmissionDTO>>() {
+            }.getType());
+            List<FormSubmission> fsl = with(formSubmissions).convert(new Converter<FormSubmissionDTO, FormSubmission>() {
+                @Override
+                public FormSubmission convert(FormSubmissionDTO submission) {
+                    return FormSubmissionConverter.toFormSubmission(submission);
+                }
+            });
+            for (FormSubmission formSubmission : fsl) {
+            	if(openmrsConnector.isOpenmrsForm(formSubmission)){
+	            	JSONObject p = patientService.getPatientByIdentifier(formSubmission.entityId());
+	        		if(p == null){
+	        			Client c = openmrsConnector.getClientFromFormSubmission(formSubmission);
+	        			System.out.println(patientService.createPatient(c));
+	        		}
+	        		Event e = openmrsConnector.getEventFromFormSubmission(formSubmission);
+	        		
+	        		System.out.println(encounterService.createEncounter(e));
+            	}
+    		}
+            }
+            catch(Exception e){
+            	e.printStackTrace();
+            }
             logger.debug(format("Added Form submissions to queue.\nSubmissions: {0}", formSubmissionsDTO));
         } catch (Exception e) {
             logger.error(format("Form submissions processing failed with exception {0}.\nSubmissions: {1}", e, formSubmissionsDTO));
