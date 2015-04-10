6e460610b06d122f86bb9625bd29149d3f82656b
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-04-10 12:07:52 +0500

    MK: class to load openmrs properties from opersrp.properties file for tests

---
+++ opensrp-connector/src/test/java/TestResourceLoader.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+
+public class TestResourceLoader {
+	protected String openmrsOpenmrsUrl;
+	protected String openmrsUsername;
+	protected String openmrsPassword;
+
+	public TestResourceLoader() throws IOException {
+		Resource resource = new ClassPathResource("/opensrp.properties");
+		Properties props = PropertiesLoaderUtils.loadProperties(resource);
+		openmrsOpenmrsUrl = props.getProperty("openmrs.url");
+		openmrsUsername = props.getProperty("openmrs.username");
+		openmrsPassword = props.getProperty("openmrs.password");
+	}
+}
