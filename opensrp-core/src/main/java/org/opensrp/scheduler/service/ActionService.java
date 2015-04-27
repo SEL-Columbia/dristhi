43a1c3c76e6749a37bc0764d5c4364d24981506b
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-04-27 10:46:42 +0500

    MK:moved

---
+++ opensrp-core/src/main/java/org/opensrp/scheduler/service/ActionService.java
@@ -0,0 +1,61 @@
+package org.opensrp.scheduler.service;
+
+import static org.opensrp.dto.BeneficiaryType.child;
+import static org.opensrp.dto.BeneficiaryType.ec;
+import static org.opensrp.dto.BeneficiaryType.mother;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.opensrp.dto.ActionData;
+import org.opensrp.dto.AlertStatus;
+import org.opensrp.dto.BeneficiaryType;
+import org.opensrp.scheduler.Action;
+import org.opensrp.scheduler.repository.AllActions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActionService {
+    private AllActions allActions;
+
+    @Autowired
+    public ActionService(AllActions allActions) {
+        this.allActions = allActions;
+    }
+
+    public List<Action> getNewAlertsForANM(String anmIdentifier, long timeStamp) {
+        return allActions.findByANMIDAndTimeStamp(anmIdentifier, timeStamp);
+    }
+
+    public void alertForBeneficiary(BeneficiaryType beneficiaryType, String caseID, String anmIdentifier, String scheduleName, String visitCode, AlertStatus alertStatus, DateTime startDate, DateTime expiryDate) {
+    	if (!(mother.equals(beneficiaryType)||child.equals(beneficiaryType)||ec.equals(beneficiaryType))) {
+            throw new IllegalArgumentException("Beneficiary Type : " + beneficiaryType + " is of unknown type");
+        }
+    	allActions.addOrUpdateAlert(new Action(caseID, anmIdentifier, ActionData.createAlert(beneficiaryType, scheduleName, visitCode, alertStatus, startDate, expiryDate)));
+    }
+
+    public void markAllAlertsAsInactive(String entityId) {
+        allActions.markAllAsInActiveFor(entityId);
+    }
+
+    public void markAlertAsInactive(String anmId, String entityId, String scheduleName) {
+        allActions.markAlertAsInactiveFor(anmId, entityId, scheduleName);
+    }
+
+    public void markAlertAsClosed(String caseId, String anmIdentifier, String visitCode, String completionDate) {
+        allActions.add(new Action(caseId, anmIdentifier, ActionData.markAlertAsClosed(visitCode, completionDate)));
+    }
+
+    public void closeMother(String caseId, String anmIdentifier, String reasonForClose) {
+        allActions.add(new Action(caseId, anmIdentifier, ActionData.closeMother(reasonForClose)));
+    }
+
+    public void reportForIndicator(String anmIdentifier, ActionData actionData) {
+        allActions.add(new Action("", anmIdentifier, actionData));
+    }
+
+    public void deleteReportActions() {
+        allActions.deleteAllByTarget("report");
+    }
+}
