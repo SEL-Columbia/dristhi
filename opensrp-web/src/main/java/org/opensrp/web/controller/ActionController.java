89f969507b4e80c5ad72e43a36e1f724903ca00d
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-04-27 11:08:18 +0500

    MK: Using new scheduling core api

--- opensrp-web/src/main/java/org/opensrp/web/controller/ActionController.java
+++ opensrp-web/src/main/java/org/opensrp/web/controller/ActionController.java
@@ -1,7 +1,8 @@
 package org.opensrp.web.controller;
 
 import ch.lambdaj.function.convert.Converter;
-import org.opensrp.service.ActionService;
+
+import org.opensrp.scheduler.service.ActionService;
 import org.opensrp.dto.Action;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -26,10 +27,10 @@ public class ActionController {
     @RequestMapping(method = RequestMethod.GET, value = "/actions")
     @ResponseBody
     public List<Action> getNewActionForANM(@RequestParam("anmIdentifier") String anmIdentifier, @RequestParam("timeStamp") Long timeStamp){
-        List<org.opensrp.domain.Action> actions = actionService.getNewAlertsForANM(anmIdentifier, timeStamp);
-        return with(actions).convert(new Converter<org.opensrp.domain.Action, Action>() {
+        List<org.opensrp.scheduler.Action> actions = actionService.getNewAlertsForANM(anmIdentifier, timeStamp);
+        return with(actions).convert(new Converter<org.opensrp.scheduler.Action, Action>() {
             @Override
-            public Action convert(org.opensrp.domain.Action action) {
+            public Action convert(org.opensrp.scheduler.Action action) {
                 return ActionConvertor.from(action);
             }
         });
