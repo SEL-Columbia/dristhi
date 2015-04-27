8b65e3026facccf145b7325fb60876475274587a
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-04-27 10:24:16 +0500

    MK: FormSubmissions and forms centrally managed via TestResourceLoader and removed repeating functionality

--- opensrp-connector/src/test/java/EncounterTest.java
+++ opensrp-connector/src/test/java/EncounterTest.java
@@ -31,8 +31,6 @@ public class EncounterTest extends TestResourceLoader{
 	PatientService ps;
 	UserService us;
 
-	String formDirPath = "form/";
-	
 	@Before
 	public void setup() throws IOException{
 		ps = new PatientService(openmrsOpenmrsUrl, openmrsUsername, openmrsPassword);
@@ -45,45 +43,35 @@ public class EncounterTest extends TestResourceLoader{
 	}
 	
 	@Test
-	public void testEncounter() throws JSONException, ParseException {
-		String fsStr = "{\"anmId\": \"admin\",   \"clientVersion\": \"1427951221623\", "
-    			+ " \"entityId\": \"b6d2751f-3c44-48d0-822c-734ee03c8aa2\","
-    			+ " \"formDataDefinitionVersion\": \"1\","
-    			+ " \"formInstance\": {\"form_data_definition_version\":\"1\",\"form\":{\"default_bind_path\":\"/model/instance/pnc_first_registration_motherdetails\",\"bind_type\":\"demo_mother\",\"fields\":[{\"name\":\"id\",\"shouldLoadValue\":true,\"source\":\"demo_mother.id\",\"value\":\"b6d2751f-3c44-48d0-822c-734ee03c8aa2\"},{\"name\":\"location\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/location\",\"source\":\"demo_mother.location\",\"value\":\"tejgaon\"},{\"name\":\"today\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/today\",\"source\":\"demo_mother.today\",\"value\":\"2015-04-02\"},{\"name\":\"start\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/start\",\"source\":\"demo_mother.start\",\"value\":\"2015-04-02T05:04:52.000-00:00\"},{\"name\":\"end\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/end\",\"source\":\"demo_mother.end\",\"value\":\"2015-04-02T05:04:52.000-00:00\"},{\"name\":\"mother_id\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/mother_id\",\"source\":\"demo_mother.mother_id\",\"value\":\"145\"},{\"name\":\"gender\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/gender\",\"source\":\"demo_mother.gender\",\"value\":\"male\"},{\"name\":\"mother_birthdate\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/mother_birthdate\",\"source\":\"demo_mother.mother_birthdate\",\"value\":\"1989-01-10\"},{\"name\":\"mother_first_name\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/mother_first_name\",\"source\":\"demo_mother.mother_first_name\",\"value\":\"jannat\"},{\"name\":\"mother_last_name\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/mother_last_name\",\"source\":\"demo_mother.mother_last_name\",\"value\":\"rehana\"},{\"name\":\"delivery_date\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/delivery_date\",\"source\":\"demo_mother.delivery_date\",\"value\":\"2015-03-30\"},{\"name\":\"delivery_facility_name\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/delivery_facility_name\",\"source\":\"demo_mother.delivery_facility_name\",\"value\":\"memon hospital\"},{\"name\":\"delivery_skilled\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/delivery_skilled\",\"source\":\"demo_mother.delivery_skilled\",\"value\":\"yes\"},{\"name\":\"delivery_type\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/delivery_type\",\"source\":\"demo_mother.delivery_type\",\"value\":\"cesarean\"},{\"name\":\"delivery_outcome\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/delivery_outcome\",\"source\":\"demo_mother.delivery_outcome\",\"value\":\"live_birth\"},{\"name\":\"parity\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/parity\",\"source\":\"demo_mother.parity\",\"value\":\"1\"},{\"name\":\"num_livebirths\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/num_livebirths\",\"source\":\"demo_mother.num_livebirths\",\"value\":\"1\"},{\"name\":\"num_stillbirths\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/num_stillbirths\",\"source\":\"demo_mother.num_stillbirths\"},{\"name\":\"complications\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/complications\",\"source\":\"demo_mother.complications\"},{\"name\":\"woman_survived\",\"bind\":\"/model/instance/pnc_first_registration_motherdetails/woman_survived\",\"source\":\"demo_mother.woman_survived\",\"value\":\"yes\"}]}},"
-    			+ " \"formName\": \"pnc_1st_registration\",  \"instanceId\": \"759b4790-47eb-457f-a5b9-3015bc89253c\"  }";
-	
-		FormSubmission fs = new Gson().fromJson(fsStr, FormSubmission.class);
-
-		System.out.println(oc.isOpenmrsForm(fs));
+	public void testEncounter() throws JSONException, ParseException, IOException {
+		FormSubmission fs = getFormSubmissionFor("basic_reg");
 		
 		JSONObject p = ps.getPatientByIdentifier(fs.entityId());
 		if(p == null){
 			Client c = oc.getClientFromFormSubmission(fs);
-			System.out.println(ps.createPatient(c));
+			//System.out.println(ps.createPatient(c));
 		}
 		Event e = oc.getEventFromFormSubmission(fs);
 		
-		System.out.println(s.createEncounter(e));
+		//System.out.println(s.createEncounter(e));
 	}
 	
 	@Test
 	public void shouldMapAddressWithClient() throws IOException, ParseException, JSONException{
 		String field = "birthplace_street";
 
-		ResourceLoader loader=new DefaultResourceLoader();
-		formDirPath  = loader.getResource(formDirPath).getURI().getPath();
-		File fsfile = new File(formDirPath+"basic_reg/formSubmission.json");
-		FormSubmission fs = new Gson().fromJson(new FileReader(fsfile), FormSubmission.class);
+		FormSubmission fs = getFormSubmissionFor("basic_reg");
+
 		System.out.println(oc.isOpenmrsForm(fs));
 		
 		JSONObject p = ps.getPatientByIdentifier(fs.entityId());
 		if(p == null){
 			Client c = oc.getClientFromFormSubmission(fs);
-			System.out.println(ps.createPatient(c));
+			//System.out.println(ps.createPatient(c));
 		}
 		Event e = oc.getEventFromFormSubmission(fs);
 		
-		System.out.println(s.createEncounter(e));
+		//System.out.println(s.createEncounter(e));
 	}
 	
 }
