8b7eb61cc7a3f90971cb261a7bd4318d2b4722f4
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-05-04 09:18:55 +0500

    MK: Added with Enums and helper methods to centralize and standardize Openmrs specific person and event attributes

---
+++ opensrp-connector/src/main/java/org/opensrp/connector/openmrs/constants/OpenmrsConstants.java
@@ -0,0 +1,41 @@
+package org.opensrp.connector.openmrs.constants;
+
+public class OpenmrsConstants {
+
+	public interface OpenmrsEntity {
+		public String entity();
+		public String entityId();
+		
+	}
+	
+	public enum Person implements OpenmrsEntity{
+		first_name,
+		middle_name,
+		last_name,
+		gender,
+		birthdate,
+		birthdate_estimated,
+		dead,
+		deathdate,
+		deathdate_estimated;
+		
+		public String entity(){return "person";}
+		public String entityId(){return this.name();}
+	}
+	
+	public enum PersonAddress implements OpenmrsEntity{
+		;
+		public String entity(){return "person_address";}
+		public String entityId(){return this.name();}
+	}
+	
+	public enum Encounter implements OpenmrsEntity{
+		encounter_date,
+		location_id,
+		encounter_start,
+		encounter_end;
+		
+		public String entity(){return "encounter";}
+		public String entityId(){return this.name();}
+	}
+}
