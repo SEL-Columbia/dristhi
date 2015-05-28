41fdd5a578ffd4f5eeda1a2f4f919efcce920f63
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-05-28 08:38:39 +0500

    MK: locations fetched from team management module first then from person attribute if not found

--- opensrp-web/src/main/java/org/opensrp/web/controller/UserController.java
+++ opensrp-web/src/main/java/org/opensrp/web/controller/UserController.java
@@ -6,12 +6,14 @@ import static org.springframework.http.HttpStatus.OK;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.opensrp.api.domain.Location;
+import org.json.JSONObject;
 import org.opensrp.api.domain.User;
 import org.opensrp.api.util.LocationTree;
 import org.opensrp.common.domain.UserDetail;
 import org.opensrp.connector.openmrs.service.OpenmrsLocationService;
+import org.opensrp.connector.openmrs.service.OpenmrsUserService;
 import org.opensrp.web.security.DrishtiAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,17 +27,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
+import com.mysql.jdbc.StringUtils;
 
 @Controller
 public class UserController {
     private String opensrpSiteUrl;
     private DrishtiAuthenticationProvider opensrpAuthenticationProvider;
 	private OpenmrsLocationService openmrsLocationService;
+	private OpenmrsUserService openmrsUserService;
 	
     @Autowired
-    public UserController(OpenmrsLocationService openmrsLocationService,
+    public UserController(OpenmrsLocationService openmrsLocationService, OpenmrsUserService openmrsUserService, 
             DrishtiAuthenticationProvider opensrpAuthenticationProvider) {
 		this.openmrsLocationService = openmrsLocationService;
+		this.openmrsUserService = openmrsUserService;
         this.opensrpAuthenticationProvider = opensrpAuthenticationProvider;
     }
 
@@ -59,16 +64,28 @@ public class UserController {
 	@ResponseBody
 	public ResponseEntity<String> authenticate() throws JSONException {
         User u = currentUser();
-        
-        String lid = (String) u.getBaseEntity().getAttribute("Location");
-        if(lid == null){
-            String lids = (String) u.getBaseEntity().getAttribute("Locations");
-            
-            if(lids == null){
-            	throw new RuntimeException("User not mapped on any location. Make sure that user have a person attribute Location or Locations with uuid(s) of valid OpenMRS Location(s) separated by ;;");
-            }
-            
-            lid = lids;
+        String lid = "";
+        try{
+        	JSONObject tm = openmrsUserService.getTeamMember(u.getBaseEntity().getAttribute("_PERSON_UUID").toString());
+        	JSONArray locs = tm.getJSONArray("location");
+        	for (int i = 0; i < locs.length(); i++) {
+				lid += locs.getJSONObject(i).getString("uuid")+";;";
+			}
+        }
+        catch(Exception e){
+        	e.printStackTrace();
+        }
+        if(StringUtils.isEmptyOrWhitespaceOnly(lid)){
+	        lid = (String) u.getBaseEntity().getAttribute("Location");
+	        if(StringUtils.isEmptyOrWhitespaceOnly(lid)){
+	            String lids = (String) u.getBaseEntity().getAttribute("Locations");
+	            
+	            if(lids == null){
+	            	throw new RuntimeException("User not mapped on any location. Make sure that user have a person attribute Location or Locations with uuid(s) of valid OpenMRS Location(s) separated by ;;");
+	            }
+	            
+	            lid = lids;
+	        }
         }
 		LocationTree l = openmrsLocationService.getLocationTreeOf(lid.split(";;"));
 		Map<String, Object> map = new HashMap<>();
