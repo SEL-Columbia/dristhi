6d940ddd83d335c45ae9c2b89f1800929cd69b30
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-11-25 11:15:57 +0500

    MK: Custom reporting code moved out of core to make code clean and readible... If needed generic reporting ever this can be added after refactorings

--- opensrp-core/src/main/java/org/opensrp/service/reporting/IMCTSReportService.java
+++
@@ -1,9 +0,0 @@
-package org.opensrp.service.reporting;
-
-import org.opensrp.form.domain.FormSubmission;
-
-public interface IMCTSReportService {
-	
-	public void reportFor(FormSubmission formSubmission) throws Exception;
-
-}
