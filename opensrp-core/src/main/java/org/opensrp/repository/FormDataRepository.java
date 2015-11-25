17f066ed9774388ee688ff9d11837bd65b68d260
Maimoona Kausar <maimoona.kausar@ihsinformatics.com>
2015-11-25 10:06:03 +0500

    MK: Not needed.. Code refactored...

--- opensrp-core/src/main/java/org/opensrp/repository/FormDataRepository.java
+++
@@ -1,6 +0,0 @@
-package org.opensrp.repository;
-
-public abstract class FormDataRepository {
-	//public abstract String saveFormSubmission(String params, String data, String formDataDefinitionVersion);
-	public abstract String saveEntity(String entityType, String fields);
-}
