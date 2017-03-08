2cd047c80614d959bfb4bb06aad6e77c70672663
Proshanto <proshanto@mpower-social.com>
2017-03-08 15:02:28 -0200

    Added Under Weight Babys

--- opensrp-connector/src/main/java/org/opensrp/connector/dhis2/DHIS2AggregateConnector.java
+++ opensrp-connector/src/main/java/org/opensrp/connector/dhis2/DHIS2AggregateConnector.java
@@ -3,10 +3,13 @@ package org.opensrp.connector.dhis2;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.opensrp.domain.Event;
+import org.opensrp.domain.Obs;
 import org.springframework.stereotype.Service;
 /*import static org.opensrp.common.AllConstants.DHIS2.*;
 */
@@ -49,10 +52,118 @@ public class DHIS2AggregateConnector extends DHIS2Service {
 	    
 	    vaccineCountObj.put("dataSet", "wn53Io9MM6B");
 		vaccineCountObj.put("completeData", modifiedDate);
-		vaccineCountObj.put("period", 201611);
+		vaccineCountObj.put("period", 201610);
 		vaccineCountObj.put("orgUnit", "IDc0HEyjhvL");
 		vaccineCountObj.put("dataValues", vaccineCountArray);
 	 return vaccineCountObj;
+	
+	}
+	
+	public JSONObject getAggregatedDataCount( List<Event> eventList) throws JSONException{
+			
+		
+		Date date = new Date();
+		String modifiedDate= new SimpleDateFormat("yyyy-MM-dd").format(date);
+		
+		Calendar now = Calendar.getInstance();
+		int year = now.get(Calendar.YEAR);
+		int month = now.get(Calendar.MONTH);
+		String periodTime =  Integer.toString(year)+Integer.toString(month);
+		Integer health=0;
+		System.out.println("periodTime:"+periodTime);
+	   	Integer birthPlaceInHealthFacilityCount=0;
+	   	Integer birthPlaceInHome=0;
+	   	Integer birthUnderWeightCount = 0;
+	   	Integer brtc = 0;
+	   	for (Event event : eventList) {
+			List<Obs> obs = event.getObs();
+				for (Obs obs2 : obs) {
+					if(obs2.getFormSubmissionField().equalsIgnoreCase("Place_Birth")){
+						List<Object> values = obs2.getHumanReadableValues();
+						for (Object object : values) {
+							if(object.toString().equalsIgnoreCase("Health facility")){
+								birthPlaceInHealthFacilityCount++;
+							}else if(object.toString().equalsIgnoreCase("Home")){
+								birthPlaceInHome++;
+							}else{
+								
+							}
+						}
+						
+					}
+					
+					if(obs2.getFormSubmissionField().equalsIgnoreCase("Birth_Weight")){
+						List<Object> values = obs2.getValues();
+						for (Object object : values) {
+							try{
+								Double value = Double.parseDouble((String) object);
+								double conditionValue =value;
+								System.out.println("conditionValue:"+conditionValue);
+								if(conditionValue <3.5){
+									birthUnderWeightCount++;
+								}else{
+									
+								}
+							}catch(Exception e){
+								System.out.println("Birth_Weight Message:"+e.getMessage());
+							}
+						}
+					}
+					
+					
+				}
+				
+				
+				
+				
+				
+				brtc++;
+		 }
+   	 	
+   	 	JSONArray vaccineDataValues =	new JSONArray();		
+		
+		
+		/**
+		 * Count for Birth place of Health_Facility of current month
+		 * */
+		JSONObject birhtPlaceInHealthFacility = new JSONObject();
+		birhtPlaceInHealthFacility.put("dataElement", "ii7lOGQqEq5");
+		birhtPlaceInHealthFacility.put("value", birthPlaceInHealthFacilityCount);
+		/**
+		 * Count for Birth place of Home of current month
+		 * */
+		JSONObject birhtPlaceInHome = new JSONObject();
+		birhtPlaceInHome.put("dataElement", "yNWOJ0OOOQD");
+		birhtPlaceInHome.put("value", birthPlaceInHome);
+		
+		
+		/**
+		 * Count Birth Under WeightCount of current month
+		 * */
+		JSONObject birthUnderWeight = new JSONObject();
+		birthUnderWeight.put("dataElement", "Wtf7iSiQdUJ");
+		birthUnderWeight.put("value", birthUnderWeightCount);
+		
+		
+		/**
+		 *Count for Total new birth registrations  of current month
+		 * */
+		JSONObject brtcObject = new JSONObject();
+		brtcObject.put("dataElement", "xMlVHstzOgC");
+		brtcObject.put("value", brtc);
+		
+		vaccineDataValues.put(brtcObject);
+		vaccineDataValues.put(birhtPlaceInHome);
+		vaccineDataValues.put(birhtPlaceInHealthFacility);
+		vaccineDataValues.put(birthUnderWeight);
+		
+	    JSONObject vaccineDataSet =	new JSONObject();
+	    vaccineDataSet.put("dataSet", "fDoHorjO5Sr");
+	    vaccineDataSet.put("completeData", modifiedDate);
+	    vaccineDataSet.put("period", 201702);
+	    vaccineDataSet.put("orgUnit", "IDc0HEyjhvL");
+	    vaccineDataSet.put("dataValues", vaccineDataValues);
+	 return vaccineDataSet;
 	
 	}
 	public JSONObject aggredateDataSendToDHIS2(JSONObject aggregateData) throws JSONException{		
--- opensrp-web/src/main/java/org/opensrp/web/controller/DHIS2Controller.java
+++ opensrp-web/src/main/java/org/opensrp/web/controller/DHIS2Controller.java
@@ -38,52 +38,15 @@ public class DHIS2Controller {
     @ResponseBody
     public ResponseEntity<String> thisMonthDataSendTODHIS2() throws JSONException{
     	 List<Event> list = allEvents.findEventByEventTypeBetweenTwoDates("Birth Registration");
-    	 Integer health=0;
-    	 Integer birthPlace=0;
-    	 Integer brtc = 0;
-    	 for (Event event : list) {
-			List<Obs> obs = event.getObs();
-			for (Obs obs2 : obs) {
-				if(obs2.getFormSubmissionField().equalsIgnoreCase("Place_Birth")){
-					List<Object> values = obs2.getHumanReadableValues();
-					for (Object object : values) {
-						if(object.toString().equalsIgnoreCase("Health facility")){
-							birthPlace++;
-						}
-					}
-					
-				}
-			}
-			
-			
-			brtc++;
+    	 
+		JSONObject aggregatedDataSet=null;
+		try{
+			 aggregatedDataSet = dHIS2AggregateConnector.getAggregatedDataCount(list);
+			dHIS2AggregateConnector.aggredateDataSendToDHIS2(aggregatedDataSet);
+		}catch(Exception e){
+			System.out.println("Aggregated Data Message:"+e.getMessage());
 		}
-    	System.out.println("Datas:"+birthPlace);
-    	JSONArray vaccineCountArray =	new JSONArray();		
-		
-		Date date = new Date();
-		String modifiedDate= new SimpleDateFormat("yyyy-MM-dd").format(date);
-		
-		
-		JSONObject vaccineAttrObj2 = new JSONObject();
-		vaccineAttrObj2.put("dataElement", "ii7lOGQqEq5");
-		vaccineAttrObj2.put("value", birthPlace);
-		
-		JSONObject brtcObject = new JSONObject();
-		brtcObject.put("dataElement", "xMlVHstzOgC");
-		brtcObject.put("value", brtc);
-		
-		vaccineCountArray.put(brtcObject);
-	    vaccineCountArray.put(vaccineAttrObj2);
-	    JSONObject vaccineCountObj =	new JSONObject();
-	    vaccineCountObj.put("dataSet", "fDoHorjO5Sr");
-		vaccineCountObj.put("completeData", modifiedDate);
-		vaccineCountObj.put("period", 201703);
-		vaccineCountObj.put("orgUnit", "IDc0HEyjhvL");
-		vaccineCountObj.put("dataValues", vaccineCountArray);
-		System.out.println("vaccineCountObj:"+vaccineCountObj.toString());
-		dHIS2AggregateConnector.aggredateDataSendToDHIS2(vaccineCountObj);
-    	return new ResponseEntity<>(new Gson().toJson(""), HttpStatus.OK);
+    	return new ResponseEntity<>(new Gson().toJson(""+aggregatedDataSet.toString()), HttpStatus.OK);
         
     }
     
