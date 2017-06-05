80557c353d6841e5df6252577992faed77e4c4b2
bitaron <bitaron90@gmail.com>
2017-06-05 13:21:26 +0600

    Modify: `User.java` tests
    Refactor existing tests.

--- opensrp-api/src/test/java/org/opensrp/api/UserTest.java
+++ opensrp-api/src/test/java/org/opensrp/api/UserTest.java
@@ -2,6 +2,7 @@ package org.opensrp.api;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.opensrp.api.domain.User;
@@ -9,54 +10,84 @@ import org.opensrp.api.domain.User;
 public class UserTest {
 
 	@Test
-	public void userTest(){
-		User u = new User("entity id 1")
-			.withUsername("username11")
-			.withPermission("view")
-			.withPermission("edit")
-			.withPermission("add")
-			.withRole("r1")
-			.withRole("r2")
-			.withStatus("active");
+	public void testUserAttributes(){
+		User u = new User("entity id 1");
 		u.withAttribute("attr1", "strval")
 			.withAttribute("attr2", 2)
 			.withAttribute("attr3", true);
 		
-		assertEquals("", (String)u.getAttribute("attr1"), "strval");
-		assertEquals("", (int)u.getAttribute("attr2"), 2);
-		assertEquals("", (boolean)u.getAttribute("attr3"), true);
-		assertNull("", u.getAttribute("attr8"));
+		assertEquals("strval", (String)u.getAttribute("attr1"));
+		assertEquals(2, (int)u.getAttribute("attr2"));
+		assertEquals(true, (boolean)u.getAttribute("attr3"));
+		assertNull(u.getAttribute("attr8"));
 		u.removeAttribute("attr3");
-		assertNull("", u.getAttribute("attr3"));
-		assertEquals("", u.getStatus(), "active");
-		assertEquals("", u.hasPermission("view"), true);
-		assertEquals("", u.hasPermission("edit"), true);
-		assertEquals("", u.hasPermission("add"), true);
-		assertEquals("", u.hasPermission("delete"), false);
-		assertEquals("", u.hasRole("r1"), true);
-		assertEquals("", u.hasRole("r2"), true);
-		assertEquals("", u.hasRole("r4"), false);
-		assertEquals("", u.hasAdminRights(), false);
-		assertEquals("", u.isDefaultAdmin(), false);
+		assertNull(u.getAttribute("attr3"));
 
-		u.addRole("admin");
-		assertEquals("", u.hasAdminRights(), true);
-		assertEquals("", u.isDefaultAdmin(), false);
-
-		u.addRole("administrator");
-		assertEquals("", u.hasAdminRights(), true);
-		assertEquals("", u.isDefaultAdmin(), false);
-		
-		u.removeRole("admin");
-		u.setUsername("admin");
-		assertEquals("", u.hasAdminRights(), true);
-		assertEquals("", u.isDefaultAdmin(), true);
-		
-		u.removeRole("administrator");
-		assertEquals("", u.hasAdminRights(), false);
-		assertEquals("", u.isDefaultAdmin(), false);
-		
 	}
-	
+
+
+	@Test
+    public void testUserPermissions() {
+        User user = new User("entity id 1")
+                .withUsername("username11")
+                .withPermission("view")
+                .withPermission("edit")
+                .withPermission("add")
+                .withPermission("delete");
+
+        assertEquals(true, user.hasPermission("view"));
+        assertEquals(true, user.hasPermission("edit"));
+        assertEquals(true, user.hasPermission("add"));
+        assertEquals(true, user.hasPermission("delete"));
+
+        user.removePermission("delete");
+        user.removePermission("dsfda");
+
+        assertEquals(false, user.hasPermission("delete"));
+    }
+
+    @Test
+    public void testUserRole() {
+        User user = new User("entity id 1")
+                .withUsername("username11")
+                .withRole("r1")
+                .withRole("r2");
+
+        assertEquals(true, user.hasRole("r1"));
+        assertEquals(true, user.hasRole("r2"));
+        assertEquals(false, user.hasRole("r4"));
+        assertEquals(false, user.hasAdminRights());
+        assertEquals(false, user.isDefaultAdmin());
+
+        user.addRole("admin");
+        assertEquals(true, user.hasAdminRights());
+        assertEquals(false, user.isDefaultAdmin());
+
+        user.addRole("administrator");
+        assertEquals(true, user.hasAdminRights());
+        assertEquals(false, user.isDefaultAdmin());
+
+        user.removeRole("admin");
+        user.setUsername("admin");
+        assertEquals(true, user.hasAdminRights());
+        assertEquals(true, user.isDefaultAdmin());
+
+        user.removeRole("administrator");
+        assertEquals(false, user.hasAdminRights());
+        assertEquals(false, user.isDefaultAdmin());
+    }
+
+    @Test
+    public void testUserActiveness() {
+        User user = new User("entity id 1")
+                .withStatus("active");
+
+        assertEquals("active", user.getStatus() );
+
+        user.setStatus("inactive");
+
+        assertEquals("inactive", user.getStatus());
+
+    }
 	
 }
