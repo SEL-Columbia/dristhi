1ec989027fe437e7102b6d382354888784160075
bitaron <bitaron90@gmail.com>
2017-06-06 16:46:52 +0600

    Test: Test case for `addNode` function in `Tree.java`

--- opensrp-api/src/test/java/org/opensrp/api/util/TreeTest.java
+++ opensrp-api/src/test/java/org/opensrp/api/util/TreeTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 
 public class TreeTest {
 
     @Test
-    public void testAddingNode() {
+    public void testAddingNodeWithOutParent() {
         Tree<Integer, Integer> tree = new Tree<>();
         tree.addNode(1,"test", 2, null );
 
@@ -20,6 +21,52 @@ public class TreeTest {
         assertEquals("test", (String) node.getLabel());
         assertEquals(2, (int) node.getNode());
         assertNull(node.getParent());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCannotReAddExistingNode() {
+        Tree<Integer, Integer> tree = new Tree<>();
+        tree.addNode(1,"test", 2, null );
+        tree.addNode(1,"test", 2, null );
+    }
+
+    @Test
+    public void testAddingNodeWithValidParent() {
+        Tree<Integer, Integer> tree = new Tree<>();
+        tree.addNode(1,"test", 1, null );
+        tree.addNode(2, "test2", 2, 1);
+
+        TreeNode<Integer, Integer> childNode = tree.getNode(2);
+
+        assertEquals(2, (int)childNode.getId());
+        assertEquals("test2", (String) childNode.getLabel());
+        assertEquals(2, (int) childNode.getNode());
+
+        assertNotNull(childNode.getParent());
+
+        int parentNodeId = childNode.getParent();
+
+        assertEquals(1, parentNodeId);
+    }
+
+    @Test
+    public void testAddingNodeWithInvalidParent() {
+        Tree<Integer, Integer> tree = new Tree<>();
+        tree.addNode(2, "test2", 2, 1);
+
+        TreeNode<Integer, Integer> childNode = tree.getNode(2);
+
+        assertEquals(2, (int)childNode.getId());
+        assertEquals("test2", (String) childNode.getLabel());
+        assertEquals(2, (int) childNode.getNode());
+
+        assertNodeIsRoot(tree, childNode);
+
+    }
+
+    private void assertNodeIsRoot(Tree<Integer,Integer> tree, TreeNode<Integer, Integer> node) {
+        assertEquals(1 , tree.map.size());
+        assertEquals((int)node.getId(), (int)tree.map.get(node.getId()).getId());
     }
 
 }
