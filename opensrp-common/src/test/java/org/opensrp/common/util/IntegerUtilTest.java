8bdcf5443d01193204f501b4b320384ec60defb5
bitaron <bitaron90@gmail.com>
2017-06-11 15:26:13 +0600

    Test: add test for `Integer.java`.

--- opensrp-common/src/test/java/org/opensrp/common/util/IntegerUtilTest.java
+++ opensrp-common/src/test/java/org/opensrp/common/util/IntegerUtilTest.java
@@ -4,13 +4,44 @@ import org.junit.Test;
 import org.opensrp.common.util.IntegerUtil;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class IntegerUtilTest {
+    IntegerUtil integerUtil;
+
     @Test
     public void shouldParseIntSafely() throws Exception {
-        assertEquals(0, IntegerUtil.tryParse("", 0));
-        assertEquals(2, IntegerUtil.tryParse(null, 2));
-        assertEquals(0, IntegerUtil.tryParse("0", 1));
-        assertEquals(3, IntegerUtil.tryParse("3", 3));
+        assertEquals(0, integerUtil.tryParse("", 0));
+        assertEquals(2, integerUtil.tryParse(null, 2));
+        assertEquals(0, integerUtil.tryParse("0", 1));
+        assertEquals(3, integerUtil.tryParse("3", 3));
     }
+
+    @Test
+    public void isIntegerTestForValidInteger() throws Exception {
+        assertTrue(integerUtil.isInteger("2"));
+    }
+
+    @Test
+    public void isIntegerTestForInvalidInteger() throws Exception {
+        assertFalse(integerUtil.isInteger("hi"));
+    }
+
+    @Test
+    public void forValidIntergers() throws Exception {
+        String x = integerUtil.parseValidIntegersAndDefaultInvalidOnesToEmptyString("hi");
+        //System.out.println("i am null string" + x);
+        assertEquals("", x);
+    }
+
+    @Test
+    public void forInvalidIntegers() throws Exception {
+        String y = integerUtil.parseValidIntegersAndDefaultInvalidOnesToEmptyString("9");
+        //System.out.println(y);
+        assertEquals("9", y);
+
+    }
+
+
 }
