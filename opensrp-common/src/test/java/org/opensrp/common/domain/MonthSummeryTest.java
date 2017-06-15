bbdd70203d2c66aee30fe8f0c283b03a5fa86c5b
bitaron <bitaron90@gmail.com>
2017-06-15 10:48:41 +0600

    Modfify: Add test for Monthsummery.

--- opensrp-common/src/test/java/org/opensrp/common/domain/MonthSummeryTest.java
+++ opensrp-common/src/test/java/org/opensrp/common/domain/MonthSummeryTest.java
@@ -1,7 +1,37 @@
 package org.opensrp.common.domain;
 
-/**
- * Created by user on 6/12/17.
- */
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 public class MonthSummeryTest {
+
+    @Test
+    public void testEqualMethodWorksForSameType() {
+        MonthSummary m1 = new MonthSummary("e", "3", "ee", "ff", Arrays.asList("sup1", "sup2", "sup3"));
+        MonthSummary m2 = new MonthSummary("e", "3", "ee", "ff", Arrays.asList("sup1", "sup2", "sup3"));
+        MonthSummary m3 = new MonthSummary(null,null,null,null,null);
+
+        assertTrue(m1.equals(m1));
+        assertTrue(m1.equals(m2));
+        assertTrue(m2.equals(m1));
+
+        assertFalse(m1.equals(m3));
+        assertFalse(m3.equals(m1));
+    }
+
+    @Test
+    public void testEqualMethodForInvalidyTypeAndMethod() {
+        MonthSummary m1 = new MonthSummary("e", "3", "ee", "ff", Arrays.asList("sup1", "sup2", "sup3"));
+        MonthSummary m2 = null;
+        String m3 = "";
+
+        assertFalse(m1.equals(m2));
+        assertFalse(m1.equals(m3));
+    }
 }
