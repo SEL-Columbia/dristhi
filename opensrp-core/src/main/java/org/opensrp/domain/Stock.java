dfb5056f2ab016bdd8c44e0830867a7dd549b0d8
manu <ona@ona-2.local>
2017-07-14 09:25:51 +0300

    Implement sync stock data functionality

--- opensrp-core/src/main/java/org/opensrp/domain/Stock.java
+++ opensrp-core/src/main/java/org/opensrp/domain/Stock.java
@@ -12,38 +12,38 @@ public class Stock extends BaseDataObject {
 	private Long identifier;
 	
 	@JsonProperty
-    private String vaccine_type_id;
+	private String vaccine_type_id;
 	
 	@JsonProperty
-    private String transaction_type;
+	private String transaction_type;
 	
 	@JsonProperty
-    private String providerid;
+	private String providerid;
 	
 	@JsonProperty
-    private int value;
+	private int value;
 	
 	@JsonProperty
-    private Long date_created;
+	private Long date_created;
 	
 	@JsonProperty
-    private String to_from;
+	private String to_from;
 	
 	@JsonProperty
-    private String sync_status;
+	private String sync_status;
 	
-	 @JsonProperty
-    private Long date_updated;
-    
-	 @JsonProperty
+	@JsonProperty
+	private Long date_updated;
+	
+	@JsonProperty
 	private long version;
 	
 	public Stock() {
 		this.version = System.currentTimeMillis();
 	}
-
+	
 	public Stock(Long identifier, String vaccine_type_id, String transaction_type, String providerid, int value,
-			Long date_created, String to_from, String sync_status, Long date_updated, long version) {
+	    Long date_created, String to_from, String sync_status, Long date_updated, long version) {
 		this.identifier = identifier;
 		this.vaccine_type_id = vaccine_type_id;
 		this.transaction_type = transaction_type;
@@ -55,83 +55,83 @@ public class Stock extends BaseDataObject {
 		this.date_updated = date_updated;
 		this.version = version;
 	}
-
+	
 	public Long getIdentifier() {
 		return identifier;
 	}
-
+	
 	public void setIdentifier(Long identifier) {
 		this.identifier = identifier;
 	}
-
+	
 	public String getVaccine_type_id() {
 		return vaccine_type_id;
 	}
-
+	
 	public void setVaccine_type_id(String vaccine_type_id) {
 		this.vaccine_type_id = vaccine_type_id;
 	}
-
+	
 	public String getTransaction_type() {
 		return transaction_type;
 	}
-
+	
 	public void setTransaction_type(String transaction_type) {
 		this.transaction_type = transaction_type;
 	}
-
+	
 	public String getProviderid() {
 		return providerid;
 	}
-
+	
 	public void setProviderid(String providerid) {
 		this.providerid = providerid;
 	}
-
+	
 	public int getValue() {
 		return value;
 	}
-
+	
 	public void setValue(int value) {
 		this.value = value;
 	}
-
+	
 	public Long getDate_created() {
 		return date_created;
 	}
-
+	
 	public void setDate_created(Long date_created) {
 		this.date_created = date_created;
 	}
-
+	
 	public String getTo_from() {
 		return to_from;
 	}
-
+	
 	public void setTo_from(String to_from) {
 		this.to_from = to_from;
 	}
-
+	
 	public String getSync_status() {
 		return sync_status;
 	}
-
+	
 	public void setSync_status(String sync_status) {
 		this.sync_status = sync_status;
 	}
-
+	
 	public Long getDate_updated() {
 		return date_updated;
 	}
-
+	
 	public void setDate_updated(Long date_updated) {
 		this.date_updated = date_updated;
 	}
-
+	
 	public long getVersion() {
 		return version;
 	}
-
+	
 	public void setVersion(long version) {
 		this.version = version;
 	}
