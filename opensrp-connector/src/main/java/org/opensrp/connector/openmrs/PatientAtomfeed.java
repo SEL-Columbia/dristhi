fb0e0fc11dc63a00ea884ee607b74264fdd106be
Ephraim Muhia <emuhia@ona.io>
2017-08-21 17:55:42 +0300

    Patient demographic edits in OMRS do not sync to ZEIR https://github.com/OpenSRP/opensrp-client-path/issues/8
    Signed-off-by: Ephraim Muhia <emuhia@ona.io>

--- opensrp-connector/src/main/java/org/opensrp/connector/openmrs/PatientAtomfeed.java
+++ opensrp-connector/src/main/java/org/opensrp/connector/openmrs/PatientAtomfeed.java
@@ -2,7 +2,15 @@ package org.opensrp.connector.openmrs;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.lang3.StringUtils;
@@ -16,6 +24,7 @@ import org.ict4h.atomfeed.client.service.AtomFeedClient;
 import org.ict4h.atomfeed.client.service.EventWorker;
 import org.ict4h.atomfeed.transaction.AFTransactionManager;
 import org.ict4h.atomfeed.transaction.AFTransactionWork;
+import org.joda.time.DateTime;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.opensrp.connector.atomfeed.AtomfeedService;
@@ -23,33 +32,43 @@ import org.opensrp.connector.openmrs.constants.OpenmrsConstants;
 import org.opensrp.connector.openmrs.service.OpenmrsService;
 import org.opensrp.connector.openmrs.service.PatientService;
 import org.opensrp.domain.Client;
+import org.opensrp.domain.Obs;
 import org.opensrp.service.ClientService;
+import org.opensrp.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PatientAtomfeed extends OpenmrsService implements EventWorker, AtomfeedService
-{
+public class PatientAtomfeed extends OpenmrsService implements EventWorker, AtomfeedService {
+	
 	private Logger log = Logger.getLogger(getClass().getSimpleName());
+	
 	private static final String CATEGORY_URL = "/OpenSRP_Patient/recent.form";
 	
 	private AtomFeedProperties atomFeedProperties;
+	
 	private AFTransactionManager transactionManager;
+	
 	private AtomFeedClient client;
-
+	
 	private PatientService patientService;
+	
 	private ClientService clientService;
-
+	
+	private EventService eventService;
+	
 	@Autowired
-	public PatientAtomfeed(AllMarkers allMarkers, AllFailedEvents allFailedEvents, 
-			@Value("#{opensrp['openmrs.url']}") String baseUrl, PatientService patientService, ClientService clientService) throws URISyntaxException {
-		if(baseUrl != null){
+	public PatientAtomfeed(AllMarkers allMarkers, AllFailedEvents allFailedEvents,
+	    @Value("#{opensrp['openmrs.url']}") String baseUrl, PatientService patientService, ClientService clientService,
+	    EventService eventService) throws URISyntaxException {
+		if (baseUrl != null) {
 			OPENMRS_BASE_URL = baseUrl;
 		}
-
+		
 		this.atomFeedProperties = new AtomFeedProperties();
-		this.transactionManager = new AFTransactionManager(){
+		this.transactionManager = new AFTransactionManager() {
+			
 			@Override
 			public <T> T executeWithTransaction(AFTransactionWork<T> action) throws RuntimeException {
 				return action.execute();
@@ -57,58 +76,64 @@ public class PatientAtomfeed extends OpenmrsService implements EventWorker, Atom
 		};
 		WebClient webClient = new WebClient();
 		
-		URI uri = new URI(OPENMRS_BASE_URL+OpenmrsConstants.ATOMFEED_URL+CATEGORY_URL);
-		this.client = new AtomFeedClient(new AllFeeds(webClient), allMarkers, allFailedEvents, atomFeedProperties, transactionManager, uri, this);
-	
+		URI uri = new URI(OPENMRS_BASE_URL + OpenmrsConstants.ATOMFEED_URL + CATEGORY_URL);
+		this.client = new AtomFeedClient(new AllFeeds(webClient), allMarkers, allFailedEvents, atomFeedProperties,
+		        transactionManager, uri, this);
+		
 		this.patientService = patientService;
 		this.clientService = clientService;
+		this.eventService = eventService;
 	}
 	
 	@Override
 	public void process(Event event) {
-		log.info("Processing item : "+event.getContent());
+		log.info("Processing item : " + event.getContent());
 		try {
-			String content = event.getContent().substring(event.getContent().lastIndexOf("/")+1);
+			String content = event.getContent().substring(event.getContent().lastIndexOf("/") + 1);
 			JSONObject p = patientService.getPatientByUuid(content, true);
-			if(p == null){
-				throw new RuntimeException("Patient uuid specified in atomfeed content ("+content+") did not return any patient.");
+			if (p == null) {
+				throw new RuntimeException("Patient uuid specified in atomfeed content (" + content
+				        + ") did not return any patient.");
 			}
 			Client c = patientService.convertToClient(p);
 			Client existing = clientService.findClient(c);
-			if(existing == null){
+			if (existing == null) {
 				c.setBaseEntityId(UUID.randomUUID().toString());
 				clientService.addClient(c);
-
+				
 				JSONObject newId = patientService.addThriveId(c.getBaseEntityId(), p);
-				log.info("New Client -> Posted Thrive ID back to OpenMRS : "+newId);
-			}
-			else {
+				log.info("New Client -> Posted Thrive ID back to OpenMRS : " + newId);
+			} else {
 				String srpIdInOpenmrs = c.getBaseEntityId();
 				Client cmerged = clientService.mergeClient(c);
 				//TODO what if in any case thrive id is assigned to some other patient 
-				if(StringUtils.isBlank(srpIdInOpenmrs) || !srpIdInOpenmrs.equalsIgnoreCase(cmerged.getBaseEntityId())){
+				if (StringUtils.isBlank(srpIdInOpenmrs) || !srpIdInOpenmrs.equalsIgnoreCase(cmerged.getBaseEntityId())) {
 					// if openmrs doesnot have openSRP UID or have a different UID then update
 					JSONObject newId = patientService.addThriveId(cmerged.getBaseEntityId(), p);
-					log.info("Existing Client missing Valid SRP UID -> Posted Thrive ID back to OpenMRS : "+newId);
+					log.info("Existing Client missing Valid SRP UID -> Posted Thrive ID back to OpenMRS : " + newId);
 				}
+				
+				// Trigger an update to the client
+				createUpdateClientEvent(existing);
 			}
-		} catch (JSONException e) {
+		}
+		catch (JSONException e) {
 			throw new RuntimeException(e);
 		}
 	}
-
+	
 	@Override
 	public void cleanUp(Event event) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void processEvents() {
 		Logger.getLogger(getClass().getName()).info("Processing PatientAtomfeeds");
 		client.processEvents();
 	}
-
+	
 	@Override
 	public void processFailedEvents() {
 		client.processFailedEvents();
@@ -117,6 +142,7 @@ public class PatientAtomfeed extends OpenmrsService implements EventWorker, Atom
 	void setUrl(String url) {
 		OPENMRS_BASE_URL = url;
 	}
+	
 	PatientService getPatientService() {
 		return patientService;
 	}
@@ -131,6 +157,74 @@ public class PatientAtomfeed extends OpenmrsService implements EventWorker, Atom
 	
 	void setClientService(ClientService clientService) {
 		this.clientService = clientService;
+	}
+	
+	/**
+	 * Add an update birth registration event to trigger client update
+	 * 
+	 * @param client
+	 */
+	private void createUpdateClientEvent(Client client) {
+		try {
+			List<org.opensrp.domain.Event> events = eventService.findByBaseEntityId(client.getBaseEntityId());
+			org.opensrp.domain.Event birthRegEvent = null;
+			if (events != null && !events.isEmpty()) {
+				for (org.opensrp.domain.Event event : events) {
+					if (event.getEventType().equals("Birth Registration")) {
+						birthRegEvent = event;
+						break;
+					}
+					
+				}
+			}
+			
+			if (birthRegEvent == null) {
+				return;
+			}
+			
+			Date start = new Date();
+			
+			org.opensrp.domain.Event event = (org.opensrp.domain.Event) new org.opensrp.domain.Event()
+			        .withBaseEntityId(client.getBaseEntityId()).withEventDate(client.getDateEdited())
+			        .withEventType("Update Birth Registration").withLocationId(birthRegEvent.getLocationId())
+			        .withProviderId(birthRegEvent.getProviderId()).withEntityType(birthRegEvent.getEntityType())
+			        .withFormSubmissionId(UUID.randomUUID().toString()).withDateCreated(new DateTime());
+			
+			SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			
+			Map<String, String> metaFields = new HashMap<>();
+			metaFields.put("deviceid", "163149AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			metaFields.put("end", "163138AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			metaFields.put("start", "163137AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			Calendar calendar = Calendar.getInstance();
+			
+			String end = DATE_TIME_FORMAT.format(calendar.getTime());
+			
+			List<Obs> obses = new ArrayList<>();
+			Obs obs = new Obs();
+			obs.setFieldCode("163137AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			obs.setValue(DATE_TIME_FORMAT.format(start));
+			obs.setFieldType("concept");
+			obs.setFieldDataType("start");
+			obs.setFormSubmissionField("start");
+			obses.add(obs);
+			
+			obs = new Obs();
+			obs.setFieldCode("163137AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+			obs.setValue(end);
+			obs.setFieldType("concept");
+			obs.setFieldDataType("end");
+			obs.setFormSubmissionField("end");
+			obses.add(obs);
+			
+			event.setObs(obses);
+			
+			eventService.addEvent(event);
+			
+		}
+		catch (Exception e) {
+			log.log(Level.SEVERE, e.getMessage(), e);
+		}
 	}
 	
 }
