823f310f6d835cc7b9bd56f757356e93cb9544af
Samuel Githengi <samuel.githengi@gmail.com>
2018-01-10 09:50:06 +0300

    Add property enableJsonViews to toggle usage of XML Views in the apk or Json2View fetched from Server

--- opensrp-core/src/main/java/org/opensrp/domain/viewconfiguration/MainConfiguration.java
+++ opensrp-core/src/main/java/org/opensrp/domain/viewconfiguration/MainConfiguration.java
@@ -3,5 +3,16 @@ package org.opensrp.domain.viewconfiguration;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 public class MainConfiguration extends BaseConfiguration {
+	
+	@JsonProperty
+	private boolean enableJsonViews;
+	
+	public boolean getEnableJsonViews() {
+		return enableJsonViews;
+	}
+	
+	public void setEnableJsonViews(boolean enableJsonViews) {
+		this.enableJsonViews = enableJsonViews;
+	}
 	 
 }
