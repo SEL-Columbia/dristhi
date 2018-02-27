721b25cd6da00e4a7a8656f2e5fd29982dfdbe49
Vincent Karuri <vincentmkaruri@gmail.com>
2018-02-27 10:37:45 +0300

    Code clean up

--- opensrp-core/src/main/java/org/opensrp/repository/AllOrders.java
+++ opensrp-core/src/main/java/org/opensrp/repository/AllOrders.java
@@ -1,18 +1,14 @@
 package org.opensrp.repository;
 
 import org.ektorp.CouchDbConnector;
-import org.ektorp.support.View;
 import org.ektorp.util.Assert;
 import org.ektorp.util.Documents;
 import org.motechproject.dao.MotechBaseRepository;
 import org.opensrp.common.AllConstants;
 import org.opensrp.domain.Order;
-import org.opensrp.domain.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class AllOrders extends MotechBaseRepository<Order> {
@@ -33,11 +29,4 @@ public class AllOrders extends MotechBaseRepository<Order> {
     public Order findById(String id) {
         return couchDbConnector.get(Order.class, id);
     }
-
-    /*
-
-    @View(name = "all_orders", map = "function(doc) { if (doc.type === 'Order') { emit(doc.dateCreated); } }")
-    public List<Order> findAllStocks() {
-        return db.queryView(createQuery("all_orders").includeDocs(true), Order.class);
-    }*/
 }
