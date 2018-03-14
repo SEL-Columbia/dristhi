8b320d644ff83e251668fb0f8f376244d13f674b
Samuel Githengi <samuel.githengi@gmail.com>
2018-03-14 16:48:36 +0300

    UnitTest create tablespaces in one directory

--- assets/migrations/run_mybatis_migrations.sh
+++ assets/migrations/run_mybatis_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -L  "https://github.com/mybatis/migrations/releases/download/mybatis-migrations-3.3.1/mybatis-migrations-3.3.1-bundle.zip" > /tmp/mybatis-migrations-3.3.1-bundle.zip
-mkdir -p /opt/postgres
+mkdir -p /opt/postgres/data
 chown -R postgres /opt/postgres
 unzip /tmp/mybatis-migrations-3.3.1-bundle.zip -d /opt/mybatis
 export MIGRATIONS_HOME=/opt/mybatis/mybatis-migrations-3.3.1
