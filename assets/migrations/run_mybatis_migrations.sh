ab4d1c71386bbae818f932ec260d0f9933d39a52
Samuel Githengi <samuel.githengi@gmail.com>
2018-03-14 17:09:49 +0300

    Travis postgres tablespace configuration

--- assets/migrations/run_mybatis_migrations.sh
+++ assets/migrations/run_mybatis_migrations.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 curl -L  "https://github.com/mybatis/migrations/releases/download/mybatis-migrations-3.3.1/mybatis-migrations-3.3.1-bundle.zip" > /tmp/mybatis-migrations-3.3.1-bundle.zip
-mkdir -p /u01/postgres/data
+mkdir -p /u01/postgres/core
+mkdir  /u01/postgres/error
+mkdir  /u01/postgres/schedule
+mkdir  /u01/postgres/feed
+mkdir  /u01/postgres/form
 chown -R postgres /u01/postgres
 unzip /tmp/mybatis-migrations-3.3.1-bundle.zip -d /opt/mybatis
 export MIGRATIONS_HOME=/opt/mybatis/mybatis-migrations-3.3.1
