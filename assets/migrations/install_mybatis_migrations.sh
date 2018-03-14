2cc64997c924e64a4e3afa614d58f9ca8d1355af
Samuel Githengi <samuel.githengi@gmail.com>
2018-03-14 16:08:33 +0300

    Travis set install_mybatis_migrations.sh as executable. Run script as superuser

--- assets/migrations/install_mybatis_migrations.sh
+++ assets/migrations/install_mybatis_migrations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl -L  "https://github.com/mybatis/migrations/releases/download/mybatis-migrations-3.3.1/mybatis-migrations-3.3.1-bundle.zip" > /tmp/mybatis-migrations-3.3.1-bundle.zip
-sudo mkdir -p /opt/postgres
-sudo chown -R postgres /opt/postgres
-sudo unzip /tmp/mybatis-migrations-3.3.1-bundle.zip -d /opt/mybatis
+mkdir -p /opt/postgres
+chown -R postgres /opt/postgres
+unzip /tmp/mybatis-migrations-3.3.1-bundle.zip -d /opt/mybatis
 export MIGRATIONS_HOME=/opt/mybatis/mybatis-migrations-3.3.1
 MIGRATIONS=$MIGRATIONS_HOME/bin
 export PATH=$MIGRATIONS:$PATH
