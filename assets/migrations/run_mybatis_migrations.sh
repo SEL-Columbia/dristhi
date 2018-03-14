9857de336aa40066b924ea431ec17fdbbad649f9
Samuel Githengi <samuel.githengi@gmail.com>
2018-03-14 16:57:07 +0300

    Unitest  default tablespace location

--- assets/migrations/run_mybatis_migrations.sh
+++ assets/migrations/run_mybatis_migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -L  "https://github.com/mybatis/migrations/releases/download/mybatis-migrations-3.3.1/mybatis-migrations-3.3.1-bundle.zip" > /tmp/mybatis-migrations-3.3.1-bundle.zip
-mkdir -p /opt/postgres/data
-chown -R postgres /opt/postgres
+mkdir -p /u01/postgres/data
+chown -R postgres /u01/postgres
 unzip /tmp/mybatis-migrations-3.3.1-bundle.zip -d /opt/mybatis
 export MIGRATIONS_HOME=/opt/mybatis/mybatis-migrations-3.3.1
 MIGRATIONS=$MIGRATIONS_HOME/bin
