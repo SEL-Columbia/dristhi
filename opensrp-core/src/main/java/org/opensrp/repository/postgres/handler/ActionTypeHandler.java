7c969fab3c021d37dc68036413f2f3c6a72347b5
Samuel Githengi <samuel.githengi@gmail.com>
2018-03-21 14:52:37 +0300

    Use generic Type handler

---
+++ opensrp-core/src/main/java/org/opensrp/repository/postgres/handler/ActionTypeHandler.java
@@ -0,0 +1,7 @@
+package org.opensrp.repository.postgres.handler;
+
+import org.opensrp.scheduler.Action;
+
+public class ActionTypeHandler extends BaseTypeHandler<Action> {
+	
+}
--- opensrp-core/src/main/java/org/opensrp/repository/postgres/handler/EventTypeHandler.java
+++ opensrp-core/src/main/java/org/opensrp/repository/postgres/handler/EventTypeHandler.java
@@ -1,75 +1,7 @@
 package org.opensrp.repository.postgres.handler;
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.ibatis.type.JdbcType;
-import org.apache.ibatis.type.TypeHandler;
 import org.opensrp.domain.Event;
-import org.postgresql.util.PGobject;
 
-
-public class EventTypeHandler extends BaseTypeHandler implements TypeHandler<Event> {
-	
-	@Override
-	public void setParameter(PreparedStatement ps, int i, Event parameter, JdbcType jdbcType) throws SQLException {
-		try {
-			if (parameter != null) {
-				String jsonString = gson.toJson(parameter);
-				PGobject jsonObject = new PGobject();
-				jsonObject.setType("jsonb");
-				jsonObject.setValue(jsonString);
-				ps.setObject(i, jsonObject);
-			}
-		}
-		catch (Exception e) {
-			throw new SQLException(e);
-		}
-	}
-	
-	@Override
-	public Event getResult(ResultSet rs, String columnName) throws SQLException {
-		try {
-			String jsonString = rs.getString(columnName);
-			if (StringUtils.isBlank(jsonString)) {
-				return null;
-			}
-			return gson.fromJson(jsonString, Event.class);
-		}
-		catch (Exception e) {
-			throw new SQLException(e);
-		}
-	}
-	
-	@Override
-	public Event getResult(ResultSet rs, int columnIndex) throws SQLException {
-		try {
-			String jsonString = rs.getString(columnIndex);
-			if (StringUtils.isBlank(jsonString)) {
-				return null;
-			}
-			return gson.fromJson(jsonString, Event.class);
-		}
-		catch (Exception e) {
-			throw new SQLException(e);
-		}
-	}
-	
-	@Override
-	public Event getResult(CallableStatement cs, int columnIndex) throws SQLException {
-		try {
-			String jsonString = cs.getString(columnIndex);
-			if (StringUtils.isBlank(jsonString)) {
-				return null;
-			}
-			return gson.fromJson(jsonString, Event.class);
-		}
-		catch (Exception e) {
-			throw new SQLException(e);
-		}
-	}
+public class EventTypeHandler extends BaseTypeHandler<Event> {
 	
 }
